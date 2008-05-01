64d31a99c08b8f04644004c28ece21021b7d7c95
marcelloceschia <marcelloceschia@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2008-05-01 12:02:50 +0000

    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@161 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- chan_sccp.h
+++ chan_sccp.h
@@ -124,7 +124,7 @@ static inline unsigned long long bswap_64(unsigned long long x) {
 #define htoles(x) bswap_16(x)
 #endif
 
-#define SCCP_VERSION "20071111"
+#define SCCP_VERSION "svn-trunk"
 #define SCCP_LOCK_TRIES 10
 #define SCCP_LOCK_USLEEP 100
 
