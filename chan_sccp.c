84439511590bec15ba4c3ecad2214b5d2d8048d5
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2008-11-21 21:15:43 +0000

    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@394 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- chan_sccp.c
+++ chan_sccp.c
@@ -576,11 +576,11 @@ void sccp_hint_notify(sccp_channel_t * c, sccp_device_t * onedevice) {
 
 	if (!h)
 		return;
-
+	
 	hintList = l->hints;
 	if(!hintList)
 		return;
-
+	
 
 	sccp_log(10)(VERBOSE_PREFIX_3 "SCCP: HINT notify the state of the line %s \n", l->name);
 
@@ -891,18 +891,19 @@ sccp_line_t * buildLine(struct ast_variable *v, char *linename){
  * \note also used by realtime functionality to line device from \link ast_variable asterisk variable \endlink
  */
 sccp_line_t * build_lines(struct ast_variable *v) {
-	sccp_line_t 	*l;
+	sccp_line_t 	*l, *gl;
 	int 		amaflags = 0;
 	int 		secondary_dialtone_tone = 0;
+	char		*lineName = NULL;
 
 
 
 
  	l = buildLineTemplate();
  	while(v) {
-		if (!strcasecmp(v->name, "line")) {
-			ast_log(LOG_WARNING, "line => name is not allowed anymore; check sample config\n");
-  		}
+ 			if (!strcasecmp(v->name, "line")) {
+				ast_log(LOG_WARNING, "line => lineName is not allowed anymore; check sample config\n");
+  			}
 #ifdef CS_SCCP_REALTIME
 			if (!strcasecmp(v->name, "name")) {
  				if ( !ast_strlen_zero(v->value) ) {
@@ -936,7 +937,7 @@ sccp_line_t * build_lines(struct ast_variable *v) {
  				}
  			}
 #endif
-			else if (!strcasecmp(v->name, "id")) {
+			  else if (!strcasecmp(v->name, "id")) {
  				sccp_copy_string(l->id, v->value, sizeof(l->id));
 			} else if (!strcasecmp(v->name, "pin")) {
  				sccp_copy_string(l->pin, v->value, sizeof(l->pin));
@@ -1131,7 +1132,8 @@ sccp_device_t *build_devices(struct ast_variable *v) {
 	char 			*splitter, *k_exten = NULL, *k_name = NULL, *k_hint = NULL;
 	char 			k_speed[256];
 	uint8_t 		speeddial_index = 1;
-
+	char 			message[256]="";							//device message
+	int			res;
 
 	/* for button config */
 	char 			*buttonType = NULL, *buttonName = NULL, *buttonOption=NULL, *buttonArgs=NULL;
@@ -1143,9 +1145,9 @@ sccp_device_t *build_devices(struct ast_variable *v) {
 	d = buildDeviceTemplate();
 	while (v) {
 			sccp_log(10)(VERBOSE_PREFIX_3 "%s = %s\n", v->name, v->value);
- 			if (!strcasecmp(v->name, "device")) {
- 				ast_log(LOG_WARNING, "device => SEP****** is not allowed anymore; check sample config\n");
- 			}
+			if (!strcasecmp(v->name, "device")) {
+				ast_log(LOG_WARNING, "device => SEP****** is not allowed anymore; check sample config\n");
+			}
 #ifdef CS_SCCP_REALTIME
 			else if (!strcasecmp(v->name, "name")) {
 				if ( (strlen(v->value) == 15) && ((strncmp(v->value, "SEP",3) == 0) || (strncmp(v->value, "ATA",3)==0)) ) {
@@ -1260,7 +1262,7 @@ sccp_device_t *build_devices(struct ast_variable *v) {
 						sccp_copy_string(currentButton->button.speeddial.hint, ast_strip(buttonArgs), sizeof(currentButton->button.speeddial.hint));
 // 						ast_verbose(VERBOSE_PREFIX_3 "hint: '%s' \n", currentButton->button.speeddial.hint);
 					}
-
+					
 
 				} else if (!strcasecmp(buttonType, "feature") && buttonName){
 					currentButton = malloc(sizeof(sccp_buttonconfig_t));
@@ -1320,8 +1322,8 @@ sccp_device_t *build_devices(struct ast_variable *v) {
  			} else if (!strcasecmp(v->name, "tzoffset")) {
 				/* timezone offset */
 				d->tz_offset = atoi(v->value);
-// 			} else if (!strcasecmp(v->name, "autologin")) {
-// 				sccp_copy_string(d->autologin, v->value, sizeof(d->autologin));
+			} else if (!strcasecmp(v->name, "autologin")) {
+				sccp_copy_string(d->autologin, v->value, sizeof(d->autologin));
 			} else if (!strcasecmp(v->name, "description")) {
 				sccp_copy_string(d->description, v->value, sizeof(d->description));
 			} else if (!strcasecmp(v->name, "imageversion")) {
@@ -1790,7 +1792,7 @@ static int reload_config(void) {
 
 #ifndef ASTERISK_CONF_1_6
 	//cfg = ast_config_load("sccp.conf");
-	cfg_v3 = ast_config_load("sccp_v3.conf");
+	cfg_v3 = ast_config_load2("sccp_v3.conf");
 #else
 	//cfg = ast_config_load2("sccp.conf", "chan_sccp", config_flags);
 	cfg_v3 = ast_config_load2("sccp_v3.conf", "chan_sccp", config_flags);
@@ -1956,7 +1958,7 @@ void buildSoftkeyTemplate(struct ast_variable *astVar){
 			}
 			template->type = skinny_str2lbl(res);
 			//sccp_copy_string(template->type, res, sizeof(template->type));
-
+			
 		}
 		template = NULL;
 		variable = variable->next;
@@ -2168,7 +2170,7 @@ static int unload_module(void) {
 // 			l->hints = l->hints->next;
 // 			free(h);
 // 		}
-		while (l->hints) {
+		while (l->hints) {	
 			hintList = l->hints;
 			h = (sccp_hint_t*)hintList->data;
 			l->hints = l->hints->next;
