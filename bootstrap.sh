7f73d6de763cee21f6221d5a52fc8d8b43307468
davidded <davidded@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2009-11-25 16:38:45 +0000

    fixed missing whitespace in bootstrap script for autoconf
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@1043 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ -f src/Makefile]; then
+if [ -f src/Makefile ]; then
   rm -rf libtool m4 config aclocal.m4 autom4te.cache/ src/Makefile.in src/config.h src/Makefile
 fi
 if ! [ -d m4 ]; then mkdir m4;fi
