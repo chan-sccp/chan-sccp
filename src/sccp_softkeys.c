03221e6e5dfbc447fe36388f754361be3dd6714f
ddegroot <ddegroot@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2009-12-03 20:22:21 +0000

    Added Comment Headers to all files
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@1069 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_softkeys.c
+++ src/sccp_softkeys.c
@@ -1,4 +1,4 @@
-/*
+/*!
  * \file 	sccp_softkeys.c
  * \brief 	SCCP SoftKeys Class
  * \author 	Sergio Chersovani <mlists [at] c-net.it>
@@ -6,7 +6,6 @@
  *        	The original chan_sccp driver that was made by Zozo which itself was derived from the chan_skinny driver.
  *        	Modified by Jan Czmok and Julien Goodwin
  * \note 	This program is free software and may be modified and distributed under the terms of the GNU Public License.
- * \version 	$LastChangedDate$
  */
 
 #include "config.h"
