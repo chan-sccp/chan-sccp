7972088802b4eb9c60ac07b94bc82de46add2e0c
marcelloceschia <marcelloceschia@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2010-03-17 13:08:50 +0000

    debug output
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@1378 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_hint.c
+++ src/sccp_hint.c
@@ -232,7 +232,7 @@ int sccp_hint_state(char *context, char* exten, enum ast_extension_states state,
 		return 0;
 	}
 
-	sccp_log(SCCP_VERBOSE_LEVEL_HINT)(VERBOSE_PREFIX_3 "SCCP: get new hint state %s for %s\n", extensionstatus2str(state), hint->hint_dialplan);
+	sccp_log(SCCP_VERBOSE_LEVEL_HINT)(VERBOSE_PREFIX_3 "SCCP: get new hint state %s(%d) for %s\n", extensionstatus2str(state), state, hint->hint_dialplan);
 	hint->callInfo.calltype = SKINNY_CALLTYPE_OUTBOUND;
 	/* converting asterisk state -> sccp state */
 	switch(state) {
