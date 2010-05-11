373485ded5163b2cf7673db8654b63667355aded
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2010-05-11 14:46:46 +0000

    we need speeker and date-time
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@1513 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_indicate.c
+++ src/sccp_indicate.c
@@ -161,9 +161,9 @@ void __sccp_indicate_nolock(sccp_device_t *device, sccp_channel_t * c, uint8_t s
 		sccp_dev_set_keyset(d, instance, c->callid, KEYMODE_ONHOOK);
 
 
-		//sccp_handle_time_date_req(d->session, NULL);
-		//if (c == d->active_channel)
-	//		sccp_dev_set_speaker(d, SKINNY_STATIONSPEAKER_OFF);
+		sccp_handle_time_date_req(d->session, NULL);/* we need datetime on hangup for 7936 */
+		if (c == d->active_channel)
+			sccp_dev_set_speaker(d, SKINNY_STATIONSPEAKER_OFF);
 
 		if (c->previousChannelState == SCCP_CHANNELSTATE_RINGING)
 			sccp_dev_set_ringer(d, SKINNY_STATION_RINGOFF, instance, c->callid);
