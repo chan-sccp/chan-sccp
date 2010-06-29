1e1bb435941702b45c1c85a07ec1821ead84ee50
ddegroot <ddegroot@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2010-06-29 09:17:16 +0000

    Added debian package generation (make deb) Added Romain Bignon to the AUTHORS file Updated ChangeLogs
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@1553 fa19cda8-b258-4bce-aa1a-843e42da0b19

---
+++ debian/chan-sccp-b.default.ex
@@ -0,0 +1,10 @@
+# Defaults for chan-sccp-b initscript
+# sourced by /etc/init.d/chan-sccp-b
+# installed at /etc/default/chan-sccp-b by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
