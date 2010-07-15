f0dda293f6e6c2b5e4a8a3f703dec667208645c9
marcelloceschia <marcelloceschia@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2010-07-15 09:30:42 +0000

    add LineStatDynamicMessage builder
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@1589 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_utils.h
+++ src/sccp_utils.h
@@ -130,6 +130,7 @@ boolean_t sccp_util_matchSubscriptionId(const sccp_channel_t *channel, const cha
 sccp_linedevices_t *sccp_util_getDeviceConfiguration(sccp_device_t *device, sccp_line_t *line);
 uint32_t sccp_parse_debugline (char * arguments[], int startat, int argc, uint32_t new_debug);
 const char * sccp_get_debugcategories(uint32_t debugvalue,char * dest);
+sccp_moo_t *sccp_utils_buildLineStatDynamicMessage(uint32_t lineInstance, const char *dirNum, const char *fqdn, const char *lineDisplayName);
 #endif
 
 
