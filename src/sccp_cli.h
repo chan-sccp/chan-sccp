620081fa5deb5b15ab69e4b21148d61b75080805
ddegroot <ddegroot@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2010-09-07 15:17:51 +0000

    Simplified cli programming via MACRO to combat asterisk version differences Fixed sccp debug category printing Fixed sccp show line tab-completion Added new sccp_utils function implode/explode (also know as split and join)
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@1834 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_cli.h
+++ src/sccp_cli.h
@@ -17,4 +17,64 @@
 void sccp_register_cli(void);
 void sccp_unregister_cli(void);
 
-#endif
+// CLI_ENTRY
+//   param1=registration_name
+//   param2=function to execute when called
+//   param3=cli string to be types as array of strings
+//   param4=registration description
+//   param5=usage string
+#if ASTERISK_VERSION_NUM >= 10600
+  #define CLI_ENTRY_COMPLETE(_function_name,_called_function,_cli_command,_descr,_usage,_completer)	\
+	static char *_function_name(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a){	\
+		if (cmd == CLI_INIT) {								\
+			e->command = implode(_cli_command," \t");				\
+			e->usage = _usage;							\
+			return NULL;								\
+		} else if (cmd == CLI_GENERATE) 						\
+			return _completer((char *)a->line, (char *)a->word, a->pos, a->n);	\
+												\
+		if (a->argc < (int)sizeof(*_cli_command)-1) 					\
+			return CLI_SHOWUSAGE;							\
+												\
+		if(_called_function(a->fd, a->argc, a->argv) == RESULT_SUCCESS)			\
+			return CLI_SUCCESS;							\
+		else										\
+			return CLI_FAILURE;							\
+	};
+  #define CLI_ENTRY(_function_name,_called_function,_cli_command,_descr,_usage)	\
+	static char *_function_name(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a){	\
+		if (cmd == CLI_INIT) {								\
+			e->command = implode(_cli_command," \t");				\
+			e->usage = _usage;							\
+			return NULL;								\
+		} else if (cmd == CLI_GENERATE)							\
+			return NULL;								\
+												\
+		if (a->argc != sizeof(*_cli_command)-1) 					\
+			return CLI_SHOWUSAGE;							\
+												\
+		if(_called_function(a->fd, a->argc, a->argv) == RESULT_SUCCESS)			\
+			return CLI_SUCCESS;							\
+		else										\
+			return CLI_FAILURE;							\
+	};
+#else
+  #define CLI_ENTRY_COMPLETE(_function_name,_called_function,_cli_command,_descr,_usage,_completer)	\
+	static struct ast_cli_entry cli_show_lines = {						\
+	  _cli_command,										\
+	  _called_function,									\
+	  _descr,										\
+	  _usage,										\
+	  _completer										\
+	};
+  #define CLI_ENTRY(_function_name,_called_function,_cli_command,_descr,_usage,_completer)	\
+	static struct ast_cli_entry cli_show_lines = {						\
+	  _cli_command,										\
+	  _called_function,									\
+	  _descr,										\
+	  _usage										\
+	};
+#endif // ASTERISK_VERSION_NUM >= 10600
+
+
+#endif // __SCCP_CLI_H
