e320f938f8e2a76dd8e527c8c7a7d382f8d45a22
marcelloceschia <marcelloceschia@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2010-10-05 21:55:00 +0000

    shrink softkeysmap
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@1959 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_protocol.h
+++ src/sccp_protocol.h
@@ -3207,17 +3207,15 @@ static const uint8_t softkeysmap[] = {
 	SKINNY_LBL_CALLBACK,
 	SKINNY_LBL_BARGE,
 	SKINNY_LBL_DND,
-	SKINNY_LBL_NO_PARK_NUMBER_AVAILABLE, 						/* Should be: Acct */
-	SKINNY_LBL_CALLPARK_REVERSION,       						/* Should be: Flash */
-	SKINNY_LBL_SERVICE_IS_NOT_ACTIVE,    						/* Should be: Login */
-	SKINNY_LBL_EMPTY,                    						/* Should be: HLog */
 	SKINNY_LBL_CONFLIST,
 	SKINNY_LBL_SELECT,
 	SKINNY_LBL_PRIVATE,
 	SKINNY_LBL_TRNSFVM,
 	SKINNY_LBL_DIRTRFR,
 	SKINNY_LBL_IDIVERT,
+	SKINNY_LBL_VIDEO_MODE,
 	SKINNY_LBL_INTRCPT,
+	SKINNY_LBL_EMPTY,
 //	SKINNY_LBL_CBARGE,
 };											/*!< Soft Keys Map as INT */
 
@@ -3283,15 +3281,12 @@ static uint8_t skSet_OnhookStealable [] = {
         SKINNY_LBL_REDIAL,
         SKINNY_LBL_NEWCALL,
         SKINNY_LBL_CFWDALL,
-//	SKINNY_LBL_CFWDBUSY,
-//	SKINNY_LBL_CFWDNOANSWER,
 #ifdef CS_SCCP_PICKUP
         SKINNY_LBL_PICKUP,
         SKINNY_LBL_GPICKUP,
 #endif
-//	SKINNY_LBL_CONFLIST,
         SKINNY_LBL_DND,
-		SKINNY_LBL_RESUME,
+	SKINNY_LBL_INTRCPT,
 };										/*!< SKINNY SoftKeys Set "Onhook" as INT */
 
 static uint8_t skSet_Connected [] = {
