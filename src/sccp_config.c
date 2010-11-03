f64c042b8dc7dc9fca42db28accbbc96e633d3a7
progs <progs@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2010-11-03 14:04:54 +0000

    fix register of not configured devices
    when a device is registering on chan_sccp-b and is not configured in
    sccp.conf, the hotline is associated to the device, but pendingUpdate was
    set to 1, so the device was infinitely restarted.
    
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@2092 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_config.c
+++ src/sccp_config.c
@@ -166,7 +166,12 @@ void sccp_config_addButton(sccp_device_t *device, int index, button_type_t type,
 		sccp_log((DEBUGCAT_NEWCODE | DEBUGCAT_CONFIG))(VERBOSE_PREFIX_2 "New %s Button %s at : %d:%d\n", sccp_buttontype2str(type), name, index, config->index);
 		SCCP_LIST_INSERT_TAIL(&device->buttonconfig, config, list);
 		new = TRUE;
-		device->pendingUpdate = 1;
+#ifdef CS_DYNAMIC_CONFIG
+		ast_mutex_lock(&GLOB(lock));
+		if (GLOB(reload_in_progress) == TRUE)
+			device->pendingUpdate = 1;
+		ast_mutex_unlock(&GLOB(lock));
+#endif
 	}
 	else
 		config->pendingDelete = 0;
