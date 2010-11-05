ca3325673d0d67f460a8bcb2b3e0d4df35e80fee
progs <progs@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2010-11-05 15:54:28 +0000

    oops
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@2097 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_channel.c
+++ src/sccp_channel.c
@@ -164,7 +164,7 @@ void sccp_channel_updateChannelCapability(sccp_channel_t *channel){
 
 	/* Check here, because if channel->owner becomes NULL we want to do the check just after */
 	while (channel->owner && sccp_ast_channel_trylock(channel->owner)) {
-		if (sccp_channel_unlock(channel)) {
+		if (sccp_channel_unlock(channel) == 0) {
 			usleep(1);
 			sccp_channel_lock(channel);
 		} else {
