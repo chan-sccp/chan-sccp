cd5cff4003b75312a7daf11566177a32ee1fc76c
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2011-01-06 12:54:52 +0000

    fixes deadlock in sccp_read_data make sccp_session_close private
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@2216 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_socket.h
+++ src/sccp_socket.h
@@ -16,7 +16,6 @@
 #    define __SCCP_SOCKET_H
 
 void *sccp_socket_thread(void *ignore);
-void sccp_session_close(sccp_session_t * s);
 void sccp_session_sendmsg(const sccp_device_t * device, sccp_message_t t);
 int sccp_session_send(const sccp_device_t * device, sccp_moo_t * r);
 void sccp_session_reject(sccp_session_t * session, char *message);
