e1f4d7875103feaacd43902e47011458bc10dc05
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2011-01-11 15:10:57 +0000

    Fixes to sccp_socket.c (following romain's e-mail 5/1/2011) (by Marcello and Diederik)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@2225 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_socket.c
+++ src/sccp_socket.c
@@ -163,7 +163,6 @@ void destroy_session(sccp_session_t * s, uint8_t cleanupTime)
 	SCCP_LIST_REMOVE(&GLOB(sessions), s, list);
 	SCCP_RWLIST_UNLOCK(&GLOB(sessions));
 
-
 	d = s->device;
 
 	if (d) {
@@ -193,6 +192,13 @@ void destroy_session(sccp_session_t * s, uint8_t cleanupTime)
 	ast_free(s);
 }
 
+/*!
+ * \brief Socket Device Thread Exit
+ * \param s SCCP Session
+ *
+ * \callgraph
+ * \callergraph
+ */
 void sccp_socket_device_thread_exit(void *session){
 	sccp_session_t *s = (sccp_session_t *)session;
 	
@@ -203,29 +209,35 @@ void sccp_socket_device_thread_exit(void *session){
 	destroy_session(s, 10);
 }
 
-
+/*!
+ * \brief Socket Device Thread
+ * \param s SCCP Session
+ *
+ * \callgraph
+ * \callergraph
+ */
 void *sccp_socket_device_thread(void *session){
 	sccp_session_t *s = (sccp_session_t *)session;
-	
+
 	uint8_t keepaliveAdditionalTime = 10;
 	int res;
-	double maxWaitTime;
-	int pollTimeout;
-	
+
+	int maxWaitTime;
 	time_t now;
+	int pollTimeout;
+
 	sccp_moo_t *m;
 	pthread_cleanup_push(sccp_socket_device_thread_exit, session);
 
-	
-	
 	/* we increase additionalTime for wireless/slower devices */
 	if (s->device && (s->device->skinny_type == SKINNY_DEVICETYPE_CISCO7920 || s->device->skinny_type == SKINNY_DEVICETYPE_CISCO7921 || s->device->skinny_type == SKINNY_DEVICETYPE_CISCO7925 || s->device->skinny_type == SKINNY_DEVICETYPE_CISCO7975 || s->device->skinny_type == SKINNY_DEVICETYPE_CISCO7970 )) {
 		keepaliveAdditionalTime += 10;
 	}
-	
-	
+
 	while(!s->session_stop){	  
 		pthread_testcancel();
+		/* only unset session_stop when poll data processing went fine */
+		s->session_stop = 1;
 
 #ifdef CS_DYNAMIC_CONFIG
 		if (s->device) {
@@ -235,58 +247,64 @@ void *sccp_socket_device_thread(void *session){
 			ast_mutex_unlock(&GLOB(lock));
 		}
 #endif
-		
-
 		if (s->fds[0].fd > 0) {
-		  
-			/* calculate poll timout using keepalive interval */
+		  	/* calculate poll timout using keepalive interval */			
 			now = time(0);
-			if(s->device){
-				maxWaitTime = (uint32_t) (time(0) - s->lastKeepAlive);
-			}else{
-				maxWaitTime = GLOB(keepalive);
-			}
+			maxWaitTime = (s->device)? s->device->keepalive : GLOB(keepalive);
 			maxWaitTime += keepaliveAdditionalTime;
+
+			/* our calculation was in seconds, we need msecs */
+			pollTimeout = (int)(maxWaitTime - ((int)now - (int)s->lastKeepAlive) ) * 1000;
 			
-			
-			if(maxWaitTime < 0){
-				s->session_stop = 1;
-				continue;
+			if(pollTimeout < 0){
+				break;
 			}
 			
-			pollTimeout = (int)(maxWaitTime * 1000);
 			sccp_log((DEBUGCAT_SOCKET)) (VERBOSE_PREFIX_3 "%s: set poll timeout %d\n", DEV_ID_LOG(s->device), pollTimeout);
-			
+
 			res = sccp_socket_poll(s->fds, 1, pollTimeout);
-			if (res < 0) {
-				ast_log(LOG_ERROR, "SCCP poll() returned %d. errno: %s\n", errno, strerror(errno));
-				sccp_session_close(s);
-				destroy_session(s, 0);
-			} else if (res == 0) {
+			if(res > 0){
+				/* poll data processing */
 				pthread_testcancel();
-				// poll timeout
-				now = time(0);
-				if (s->device && s->device->keepalive && (now > ((s->lastKeepAlive + s->device->keepalive) + keepaliveAdditionalTime))) {
-					sccp_log((DEBUGCAT_SOCKET)) (VERBOSE_PREFIX_3 "%s: Session Keepalive %s Expired, now %s\n", DEV_ID_LOG(s->device), ctime(&s->lastKeepAlive), ctime(&now));
-					ast_log(LOG_WARNING, "%s: Dead device does not send a keepalive message in %d+%d seconds. Will be removed\n", DEV_ID_LOG(s->device), GLOB(keepalive), keepaliveAdditionalTime);
-					s->session_stop = 1;
-				}
-			} else {
-				pthread_testcancel();
+				
 				/* we have new data -> continue */
 				sccp_log((DEBUGCAT_SOCKET)) (VERBOSE_PREFIX_3 "%s: Session New Data Arriving\n", DEV_ID_LOG(s->device));
 				sccp_read_data(s);
 				while ((m = sccp_process_data(s))) {
-					if (!sccp_handle_message(m, s)) {
+					if (sccp_handle_message(m, s)) {
+						/* data processing and handling done without errors, we can continue */
+						s->session_stop = 0;
+					} else {
+						/* an unknown error occured, MS solution -> reset */
+						ast_log(LOG_ERROR, "SCCP sccp_handle_message returned an error, sending reset to device\n");
 						sccp_device_sendReset(s->device, SKINNY_DEVICE_RESTART);
-						s->session_stop = 1;
+						break;
 					}
 				}
-			}
+			} else if (res < 0) {
+				/* poll error */
+				ast_log(LOG_ERROR, "SCCP poll() returned %d. errno: %s\n", errno, strerror(errno));
+				usleep(10000);
+				break;
+			} else if (res == 0) {
+				/* poll timeout */
+				if(s->device){
+					if( now > ((s->lastKeepAlive + s->device->keepalive) + keepaliveAdditionalTime) ) {
+						sccp_log((DEBUGCAT_SOCKET)) (VERBOSE_PREFIX_3 "%s: Session Keepalive %s Expired, now %s\n", DEV_ID_LOG(s->device), ctime(&s->lastKeepAlive), ctime(&now));
+						ast_log(LOG_WARNING, "%s: Dead device does not send a keepalive message in %d+%d seconds. Will be removed\n", DEV_ID_LOG(s->device), GLOB(keepalive), keepaliveAdditionalTime);
+						break;
+					} else {
+						s->session_stop = 0;
+					}
+				} else {
+					/* new session without a device (yet!) -> registering */
+					s->session_stop = 0;
+				}
+			} 
 		} else {
 			/* session is gone */
 			sccp_log((DEBUGCAT_SOCKET)) (VERBOSE_PREFIX_3 "%s: Session is Gone\n", DEV_ID_LOG(s->device));
-			s->session_stop = 1;
+			break;
 		}
 	}
 	sccp_log((DEBUGCAT_SOCKET)) (VERBOSE_PREFIX_3 "%s: exiting thread\n", DEV_ID_LOG(s->device));
@@ -342,7 +360,7 @@ static void sccp_accept_connection(void)
 	memcpy(&s->sin, &incoming, sizeof(s->sin));
 	sccp_mutex_init(&s->lock);
 
-	s->fds[0].events = POLLIN;
+	s->fds[0].events = POLLIN | POLLPRI;
 	s->fds[0].revents = 0;
 	s->fds[0].fd = new_socket;
 	
@@ -492,10 +510,6 @@ void *sccp_socket_thread(void *ignore)
 
 	return NULL;
 }
-
-
-
-
 
 /*!
  * \brief Socket Send Message
