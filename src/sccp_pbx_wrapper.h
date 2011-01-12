abd8378a009e29b3bd406e7c4ac398a9a2956bc8
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2011-01-12 02:01:51 +0000

    Added some headers to sccp_pbx_wrapper
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@2237 43c1dc8c-776e-4df2-8ef1-6e829299be21

---
+++ src/sccp_pbx_wrapper.h
@@ -0,0 +1,13 @@
+/*!
+ * \file 	sccp_pbx_wrapper.h
+ * \brief 	SCCP PBX Wrapper Header
+ * \author 	Diederik de Groot <ddegroot [at] users.sourceforge.net>
+ * \note        This program is free software and may be modified and distributed under the terms of the GNU Public License.
+ *		See the LICENSE file at the top of the source tree.
+ *
+ * $Date: 2010-10-23 20:04:30 +0200 (Sat, 23 Oct 2010) $
+ * $Revision: 2044 $  
+ */
+#ifndef __SCCP_PBX_WRAPPER_H
+#    define __SCCP_PBX_WRAPPER_H
+#endif
