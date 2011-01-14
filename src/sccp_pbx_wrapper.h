2613aff96c0a9c48cb5107184e002618c4d7feeb
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2011-01-14 15:29:46 +0000

    Backport of ast_str2cos and ast_str2cos for asterisk 1.2 and subsequent renaming of ast_str2?os to pbx_str2?os in sccp_config.c
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@2258 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_pbx_wrapper.h
+++ src/sccp_pbx_wrapper.h
@@ -90,6 +90,8 @@ struct ast_config *pbx_config_load(const char *filename, const char *who_asked, 
 char * pbx_getformatname(format_t format);
 char * pbx_getformatname_multiple(char *buf, size_t size, format_t format);
 const char *pbx_inet_ntoa(struct in_addr ia);
+int pbx_str2cos(const char *value, unsigned int *cos);
+int pbx_str2tos(const char *value, unsigned int *tos);
 int pbx_rtp_get_peer(struct ast_rtp *rtp, struct sockaddr_in *them);
 void pbx_rtp_set_peer(struct ast_rtp *rtp, struct sockaddr_in *them);
 
