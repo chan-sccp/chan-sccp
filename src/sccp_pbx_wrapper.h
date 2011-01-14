ebecc1849c01dfb1a118b3c68ea45523d2d6fe98
ddegroot <ddegroot@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2011-01-14 13:12:19 +0000

    replacement funtion calls for sccp_config
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@2255 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_pbx_wrapper.h
+++ src/sccp_pbx_wrapper.h
@@ -78,6 +78,9 @@ int set_pbx_callerid(struct ast_channel *ast_chan, sccp_callinfo_t * callInfo);
 
 // replacement implementations
 struct ast_channel *pbx_channel_walk_locked(struct ast_channel *target);
+struct ast_ha *pbx_append_ha(const char *sense, const char *stuff, struct ast_ha *path, int *error);
+struct ast_context *pbx_context_find_or_create(struct ast_context **extcontexts, struct ast_hashtab *exttable, const char *name, const char *registrar);
+struct ast_config *pbx_config_load(const char *filename, const char *who_asked, struct ast_flags flags);
 const char *pbx_inet_ntoa(struct in_addr ia);
 int pbx_rtp_get_peer(struct ast_rtp *rtp, struct sockaddr_in *them);
 void pbx_rtp_set_peer(struct ast_rtp *rtp, struct sockaddr_in *them);
