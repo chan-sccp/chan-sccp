3a59cd6d4f423fd7b382745554f305a9528e50b6
ddegroot <ddegroot@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2011-01-22 17:07:13 +0000

    updated sccp_rtp to use socket_equals
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/trunk@2280 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- src/sccp_utils.h
+++ src/sccp_utils.h
@@ -138,5 +138,5 @@ boolean_t implode(char *str[], char *sep, char **res);
 #    ifdef HAVE_LIBGC
 void gc_warn_handler(char *msg, GC_word p);
 #    endif
-int socket_equals(struct sockaddr_in s0, struct sockaddr_in s1);
+int socket_equals(struct sockaddr_in *s0, struct sockaddr_in *s1);
 #endif
