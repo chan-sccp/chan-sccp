037ba4b713ec43867e568a6a262d80404edd4989
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2011-02-06 15:05:08 +0000

    Updated UNKNOWN NUMBER Indication Renamed SCCP_RTP_STATUS_PROGESS_TRANSMIT to SCCP_RTP_STATUS_PROGRESS_TRANSMIT
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@2336 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_indicate.c
+++ src/sccp_indicate.c
@@ -408,16 +408,18 @@ void __sccp_indicate_locked(sccp_device_t * device, sccp_channel_t * c, uint8_t 
 		break;
 	case SCCP_CHANNELSTATE_INVALIDNUMBER:
 		/* this is for the earlyrtp. The 7910 does not play tones if a rtp stream is open */
+		sccp_dev_displayprompt(d, instance, c->callid, SKINNY_DISP_UNKNOWN_NUMBER, 0);
 		if (d->earlyrtp) {
 			if (!c->rtp.audio.rtp)
 				sccp_channel_openreceivechannel_locked(c);
-		} else {
-			if (c->rtp.audio.rtp) {
-				sccp_channel_closereceivechannel_locked(c);
-				sccp_safe_sleep(100);
-			}
-			sccp_dev_starttone(d, SKINNY_TONE_REORDERTONE, instance, c->callid, 0);
+			/* Give early RTP users a chance to hear the message from the other side */
+			sccp_safe_sleep(10000);
 		}
+		if (c->rtp.audio.rtp) {
+			sccp_channel_closereceivechannel_locked(c);
+		}
+//		sccp_dev_starttone(d, SKINNY_TONE_NOSUCHNUMBERTONE, instance, c->callid, 10);
+		sccp_dev_starttone(d, SKINNY_TONE_REORDERTONE, instance, c->callid, 0);
 		
 		/* 7936 does not like the skinny ivalid message callstate */
 		/* In fact, newer firmware versions (the 8 releases for the 7960 etc.) and
@@ -426,7 +428,8 @@ void __sccp_indicate_locked(sccp_device_t * device, sccp_channel_t * c, uint8_t 
 		/*if (d->skinny_type != SKINNY_DEVICETYPE_CISCO7936)
 		   sccp_channel_send_callinfo(d, c); 
 		*/
-		sccp_dev_displayprompt(d, instance, c->callid, SKINNY_DISP_UNKNOWN_NUMBER, 0);
+ 		/* this is for the earlyrtp. The 7910 does not play tones if a rtp stream is open */
+		sccp_channel_send_callinfo(d, c);
 
 		/* don't set AST_STATE_DOWN. we hangup only on onhook and endcall softkey */
 		break;
