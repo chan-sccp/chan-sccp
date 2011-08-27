4c9cc43af42ca22255c9396e9e7db9a18de29c24
ddegroot <ddegroot@fa19cda8-b258-4bce-aa1a-843e42da0b19>
2011-08-27 13:03:58 +0000

    Update field length for earlyrtp in default database files
    git-svn-id: https://chan-sccp-b.svn.sourceforge.net/svnroot/chan-sccp-b/branches/V3.0@2701 fa19cda8-b258-4bce-aa1a-843e42da0b19

--- conf/database_updates/migrate_to_mysql-v5.sql
+++ conf/database_updates/migrate_to_mysql-v5.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `sccpdevice` ADD `softkeyset` VARCHAR( 50 ) NULL DEFAULT NULL AFTER `addon`;
-
+ALTER TABLE `sccpdevice` CHANGE COLUMN 'earlyrtp' VARCHAR(8) default 'off';
 ALTER TABLE `sccpline` CHANGE COLUMN `rtptos` `audio_tos` VARCHAR( 11 ) NULL DEFAULT "0xB8";
 ALTER TABLE `sccpline` ADD `audio_cos` VARCHAR( 1 ) NULL DEFAULT "6" AFTER `audio_tos`;
 ALTER TABLE `sccpline` ADD `video_tos` VARCHAR( 11 ) NULL DEFAULT "0x88" AFTER `audio_cos`;
--- conf/database_updates/update_mysql_v4.sql
+++ conf/database_updates/update_mysql_v4.sql
@@ -1,6 +1,7 @@
 ALTER TABLE `sccpdevice` ADD `softkeyset` VARCHAR( 50 ) NULL DEFAULT NULL AFTER `addon`;
 ALTER TABLE `sccpdevice` ADD `pickupcontext` VARCHAR( 100 ) NULL DEFAULT "" AFTER `pickupexten`;
 ALTER TABLE `sccpdevice` ADD `nat` VARCHAR( 5 ) NULL DEFAULT "no" BEFORE `name`;
+ALTER TABLE `sccpdevice` CHANGE COLUMN 'earlyrtp' VARCHAR(8) default 'off';
 
 ALTER TABLE `sccpline` CHANGE COLUMN `rtptos` `audio_tos` VARCHAR( 11 ) NULL DEFAULT "0xB8";
 ALTER TABLE `sccpline` ADD `audio_cos` VARCHAR( 1 ) NULL DEFAULT "6" AFTER `audio_tos`;
