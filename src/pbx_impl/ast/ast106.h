bffdd47c68b6e6600d485905df13e5014dc4fa25
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2011-09-26 16:30:13 +0000

    Changed cli completer implementation to allow more than one
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@2865 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast106.h
+++ src/pbx_impl/ast/ast106.h
@@ -53,44 +53,44 @@ int sccp_wrapper_asterisk16_requestHangup(PBX_CHANNEL_TYPE * channel);
 //   param4=registration description
 //   param5=usage string
 
-#    define CLI_AMI_OUTPUT(fd, s, ...) 								\
-	if (NULL != s) {									\
-		astman_append(s, __VA_ARGS__);							\
-		local_total++;									\
-	} else {										\
-		ast_cli(fd, __VA_ARGS__);							\
+#    define CLI_AMI_OUTPUT(fd, s, ...) 										\
+	if (NULL != s) {											\
+		astman_append(s, __VA_ARGS__);									\
+		local_total++;											\
+	} else {												\
+		ast_cli(fd, __VA_ARGS__);									\
 	}
 
-#    define CLI_AMI_OUTPUT_PARAM(param, width, fmt, ...) 					\
-	if (NULL != s) {									\
-		astman_append(s, "%s: " fmt "\r\n", param, __VA_ARGS__);			\
-		local_total++;									\
-	} else {										\
-		ast_cli(fd, "%-*.*s %s " fmt "\n", width, width, param, ":", __VA_ARGS__);	\
+#    define CLI_AMI_OUTPUT_PARAM(param, width, fmt, ...) 							\
+	if (NULL != s) {											\
+		astman_append(s, "%s: " fmt "\r\n", param, __VA_ARGS__);					\
+		local_total++;											\
+	} else {												\
+		ast_cli(fd, "%-*.*s %s " fmt "\n", width, width, param, ":", __VA_ARGS__);			\
 	}
 
-#    define CLI_AMI_OUTPUT_BOOL(param, width, value) 						\
-	if (NULL != s) {									\
-		astman_append(s, "%s: %s\r\n", param, ((value) ? "on" : "off"));		\
-		local_total++;									\
-	} else {										\
-		ast_cli(fd, "%-*.*s %s %s\n", width, width, param, ":", ((value) ? "on" : "off")); \
+#    define CLI_AMI_OUTPUT_BOOL(param, width, value) 								\
+	if (NULL != s) {											\
+		astman_append(s, "%s: %s\r\n", param, ((value) ? "on" : "off"));				\
+		local_total++;											\
+	} else {												\
+		ast_cli(fd, "%-*.*s %s %s\n", width, width, param, ":", ((value) ? "on" : "off")); 		\
 	}
-#    define CLI_AMI_OUTPUT_YES_NO(param, width, value) 						\
-	if (NULL != s) {									\
-		astman_append(s, "%s: %s\r\n", param, ((value) ? "yes" : "no"));		\
-		local_total++;									\
-	} else {										\
-		ast_cli(fd, "%-*.*s %s %s\n", width, width, param, ":", ((value) ? "yes" : "no")); \
+#    define CLI_AMI_OUTPUT_YES_NO(param, width, value) 								\
+	if (NULL != s) {											\
+		astman_append(s, "%s: %s\r\n", param, ((value) ? "yes" : "no"));				\
+		local_total++;											\
+	} else {												\
+		ast_cli(fd, "%-*.*s %s %s\n", width, width, param, ":", ((value) ? "yes" : "no")); 		\
 	}
-#    define CLI_AMI_ERROR(fd, s, m, fmt, ...) 							\
-	pbx_log(LOG_WARNING, "SCCP CLI ERROR" fmt, __VA_ARGS__);				\
-	if (NULL != s) {									\
-		astman_send_error(s, m, fmt);							\
-		local_total++;									\
-	} else {										\
-		ast_cli(fd, "SCCP CLI ERROR" fmt, __VA_ARGS__);					\
-	}											\
+#    define CLI_AMI_ERROR(fd, s, m, fmt, ...) 									\
+	pbx_log(LOG_WARNING, "SCCP CLI ERROR" fmt, __VA_ARGS__);						\
+	if (NULL != s) {											\
+		astman_send_error(s, m, fmt);									\
+		local_total++;											\
+	} else {												\
+		ast_cli(fd, "SCCP CLI ERROR" fmt, __VA_ARGS__);							\
+	}													\
 	return RESULT_FAILURE;
 
 // CLI_ENTRY
@@ -99,152 +99,160 @@ int sccp_wrapper_asterisk16_requestHangup(PBX_CHANNEL_TYPE * channel);
 //   param3=cli string to be types as array of strings
 //   param4=registration description
 //   param5=usage string
-#    define CLI_AMI_ENTRY_COMPLETE(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE,_COMPLETER, _POS)\
-	static int manager_ ## _FUNCTION_NAME(struct mansession *s, const struct message *m)	\
-	{											\
-		const char *id = astman_get_header(m, "ActionID");				\
-		static char *cli_ami_command[] = { CLI_COMMAND, NULL};				\
-		static char *ami_command = AMI_COMMAND;						\
-		cli_ami_command[0] = ami_command;						\
-		static char *cli_ami_params[] = { CLI_AMI_PARAMS, NULL };			\
-		static char *arguments[ARRAY_LEN(cli_ami_params)];				\
-		uint8_t x=0;									\
-		for (x=0;x<ARRAY_LEN(cli_ami_params);x++) {					\
-			arguments[x]=strdup(astman_get_header(m, cli_ami_params[x]));		\
-		}										\
-		char idtext[256] = "";								\
-		int total = 0;									\
-		if (!pbx_strlen_zero(id))							\
-			snprintf(idtext, sizeof(idtext), "ActionID: %s\r\n", id);		\
-		astman_send_ack(s, m, AMI_COMMAND);						\
-		_CALLED_FUNCTION(-1, &total, s, m, ARRAY_LEN(arguments), arguments);		\
-		astman_append(s,								\
-		"Event: " _DESCR " Complete\r\n"						\
-		"EventList: Complete\r\n"							\
-		"ListItems: %d\r\n"								\
-		"%s"										\
-		"\r\n\r\n", total, idtext);  							\
-		return 0;									\
-	}											\
-												\
-	static char * cli_ ## _FUNCTION_NAME(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a) {\
-		static char *cli_command[] = { CLI_COMMAND, NULL };					\
-		char *command=NULL;								\
-		if (cmd == CLI_INIT) {								\
-			if(!implode( cli_command," ", &command)) {				\
-				ast_free(command);						\
-				return CLI_FAILURE;						\
-			}									\
-			e->command = strdup(command);						\
-			e->usage = _USAGE;							\
-			ast_free(command);							\
-			return NULL;								\
-		} else if (cmd == CLI_GENERATE) {						\
-			if (a->pos==_POS || (0==_POS && (unsigned)a->pos > ARRAY_LEN(cli_command))) 	\
-				return _COMPLETER((char *)a->line, (char *)a->word, a->pos, a->n);\
-			else 									\
-				return NULL;							\
-		}										\
-												\
-		if (a->argc < (int)(ARRAY_LEN(cli_command))) 					\
-			return CLI_SHOWUSAGE;							\
-												\
-		if(_CALLED_FUNCTION(a->fd, NULL, NULL, NULL, a->argc, (char **) a->argv) == RESULT_SUCCESS)	\
-			return CLI_SUCCESS;							\
-		else										\
-			return CLI_FAILURE;							\
-	};
-#    define CLI_AMI_ENTRY(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE)			\
-	static int manager_ ## _FUNCTION_NAME(struct mansession *s, const struct message *m)	\
-	{											\
-		const char *id = astman_get_header(m, "ActionID");				\
-		static char *ami_command[] = { AMI_COMMAND, NULL };				\
-		char idtext[256] = "";								\
-		int total = 0;									\
-		if (!pbx_strlen_zero(id))							\
-			snprintf(idtext, sizeof(idtext), "ActionID: %s\r\n", id);		\
-		astman_send_ack(s, m, AMI_COMMAND);						\
-		_CALLED_FUNCTION(-1, &total, s, m, ARRAY_LEN(ami_command), ami_command);	\
-		astman_append(s,								\
-		"Event: " _DESCR " Complete\r\n"						\
-		"EventList: Complete\r\n"							\
-		"ListItems: %d\r\n"								\
-		"%s"										\
-		"\r\n\r\n", total, idtext);  							\
-		return 0;									\
-	}											\
+#    define CLI_AMI_ENTRY_COMPLETE(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE)				\
+	static int manager_ ## _FUNCTION_NAME(struct mansession *s, const struct message *m)			\
+	{													\
+		const char *id = astman_get_header(m, "ActionID");						\
+		static char *cli_ami_command[] = { CLI_COMMAND, NULL};						\
+		static char *ami_command = AMI_COMMAND;								\
+		cli_ami_command[0] = ami_command;								\
+		static char *cli_ami_params[] = { CLI_AMI_PARAMS, NULL };					\
+		static char *arguments[ARRAY_LEN(cli_ami_params)];						\
+		uint8_t x=0;											\
+		for (x=0;x<ARRAY_LEN(cli_ami_params);x++) {							\
+			arguments[x]=strdup(astman_get_header(m, cli_ami_params[x]));				\
+		}												\
+		char idtext[256] = "";										\
+		int total = 0;											\
+		if (!pbx_strlen_zero(id))									\
+			snprintf(idtext, sizeof(idtext), "ActionID: %s\r\n", id);				\
+		astman_send_ack(s, m, AMI_COMMAND);								\
+		_CALLED_FUNCTION(-1, &total, s, m, ARRAY_LEN(arguments), arguments);				\
+		astman_append(s,										\
+		"Event: " _DESCR " Complete\r\n"								\
+		"EventList: Complete\r\n"									\
+		"ListItems: %d\r\n"										\
+		"%s"												\
+		"\r\n\r\n", total, idtext);  									\
+		return 0;											\
+	}													\
+														\
 	static char * cli_ ## _FUNCTION_NAME(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a) {	\
-		static char *cli_command[] = { CLI_COMMAND, NULL };				\
-		char *command=NULL;								\
-		if (cmd == CLI_INIT) {								\
-			if(!implode( cli_command," ", &command)) {				\
-				ast_free(command);						\
-				return CLI_FAILURE;						\
-			}									\
-			e->command = strdup(command);						\
-			e->usage = _USAGE;							\
-			ast_free(command);							\
-		} else if (cmd == CLI_GENERATE)							\
-			return NULL;								\
-												\
-		if (a->argc < (int)(ARRAY_LEN(cli_command)-1))	 				\
-			return CLI_SHOWUSAGE;							\
-												\
+		static char *cli_command[] = { CLI_COMMAND, NULL };						\
+		static sccp_cli_completer_t cli_complete[] = { CLI_COMPLETE };					\
+		char *command=NULL;										\
+		uint8_t completer;										\
+		if (cmd == CLI_INIT) {										\
+			if(!implode( cli_command," ", &command)) {						\
+				ast_free(command);								\
+				return CLI_FAILURE;								\
+			}											\
+			e->command = strdup(command);								\
+			e->usage = _USAGE;									\
+			ast_free(command);									\
+			return NULL;										\
+		} else if (cmd == CLI_GENERATE) {								\
+			for (completer=0; completer<ARRAY_LEN(cli_complete); completer++) {			\
+				if ((unsigned)a->pos == (completer + ARRAY_LEN(cli_command) - 1)) {		\
+					return sccp_exec_completer(cli_complete[completer], (char *)a->line, (char *)a->word, a->pos, a->n);\
+				}										\
+			}											\
+			return NULL;										\
+		}												\
+														\
+		if (a->argc < (int)(ARRAY_LEN(cli_command))) 							\
+			return CLI_SHOWUSAGE;									\
+														\
 		if(_CALLED_FUNCTION(a->fd, NULL, NULL, NULL, a->argc, (char **) a->argv) == RESULT_SUCCESS)	\
-			return CLI_SUCCESS;							\
-		else										\
-			return CLI_FAILURE;							\
+			return CLI_SUCCESS;									\
+		else												\
+			return CLI_FAILURE;									\
 	};
-#    define CLI_ENTRY_COMPLETE(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE,_COMPLETER, _POS)	\
-	static char *_FUNCTION_NAME(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a) {	\
-		static char *cli_command[] = { CLI_COMMAND };					\
-		char *command=NULL;								\
-		if (cmd == CLI_INIT) {								\
-			if(!implode( cli_command," ", &command)) {				\
-				ast_free(command);						\
-				return CLI_FAILURE;						\
-			}									\
-			e->command = strdup(command);						\
-			e->usage = _USAGE;							\
-			ast_free(command);							\
-			return NULL;								\
-		} else if (cmd == CLI_GENERATE) {						\
-			if (a->pos==_POS || (0==_POS && (unsigned)a->pos > ARRAY_LEN(cli_command))) 	\
-				return _COMPLETER((char *)a->line, (char *)a->word, a->pos, a->n);\
-			else 									\
-				return NULL;							\
-		}										\
-												\
-		if (a->argc < (int)(ARRAY_LEN(cli_command))) 					\
-			return CLI_SHOWUSAGE;							\
-												\
-		if(_CALLED_FUNCTION(a->fd, a->argc, a->argv) == RESULT_SUCCESS)			\
-			return CLI_SUCCESS;							\
-		else										\
-			return CLI_FAILURE;							\
+#    define CLI_AMI_ENTRY(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE)					\
+	static int manager_ ## _FUNCTION_NAME(struct mansession *s, const struct message *m)			\
+	{													\
+		const char *id = astman_get_header(m, "ActionID");						\
+		static char *ami_command[] = { AMI_COMMAND, NULL };						\
+		char idtext[256] = "";										\
+		int total = 0;											\
+		if (!pbx_strlen_zero(id))									\
+			snprintf(idtext, sizeof(idtext), "ActionID: %s\r\n", id);				\
+		astman_send_ack(s, m, AMI_COMMAND);								\
+		_CALLED_FUNCTION(-1, &total, s, m, ARRAY_LEN(ami_command), ami_command);			\
+		astman_append(s,										\
+		"Event: " _DESCR " Complete\r\n"								\
+		"EventList: Complete\r\n"									\
+		"ListItems: %d\r\n"										\
+		"%s"												\
+		"\r\n\r\n", total, idtext);  									\
+		return 0;											\
+	}													\
+	static char * cli_ ## _FUNCTION_NAME(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a) {	\
+		static char *cli_command[] = { CLI_COMMAND, NULL };						\
+		char *command=NULL;										\
+		if (cmd == CLI_INIT) {										\
+			if(!implode( cli_command," ", &command)) {						\
+				ast_free(command);								\
+				return CLI_FAILURE;								\
+			}											\
+			e->command = strdup(command);								\
+			e->usage = _USAGE;									\
+			ast_free(command);									\
+		} else if (cmd == CLI_GENERATE)									\
+			return NULL;										\
+														\
+		if (a->argc < (int)(ARRAY_LEN(cli_command)-1))	 						\
+			return CLI_SHOWUSAGE;									\
+														\
+		if(_CALLED_FUNCTION(a->fd, NULL, NULL, NULL, a->argc, (char **) a->argv) == RESULT_SUCCESS)	\
+			return CLI_SUCCESS;									\
+		else												\
+			return CLI_FAILURE;									\
 	};
-#    define CLI_ENTRY(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE)			\
-	static char *_FUNCTION_NAME(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a) {	\
-		static char *cli_command[] = { CLI_COMMAND };					\
-		char *command=NULL;								\
-		if (cmd == CLI_INIT) {								\
-			if(!implode( cli_command," ", &command)) {				\
-				ast_free(command);						\
-				return CLI_FAILURE;						\
-			}									\
-			e->command = strdup(command);						\
-			e->usage = _USAGE;							\
-			ast_free(command);							\
-		} else if (cmd == CLI_GENERATE)							\
-			return NULL;								\
-												\
-		if (a->argc < (int)(ARRAY_LEN(cli_command)-1))	 				\
-			return CLI_SHOWUSAGE;							\
-												\
-		if(_CALLED_FUNCTION(a->fd, a->argc, a->argv) == RESULT_SUCCESS)			\
-			return CLI_SUCCESS;							\
-		else										\
-			return CLI_FAILURE;							\
+#    define CLI_ENTRY_COMPLETE(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE)					\
+	static char *_FUNCTION_NAME(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a) {			\
+		static char *cli_command[] = { CLI_COMMAND };							\
+		static sccp_cli_completer_t cli_complete[] = { CLI_COMPLETE };					\
+		char *command=NULL;										\
+		uint8_t completer;										\
+		if (cmd == CLI_INIT) {										\
+			if(!implode( cli_command," ", &command)) {						\
+				ast_free(command);								\
+				return CLI_FAILURE;								\
+			}											\
+			e->command = strdup(command);								\
+			e->usage = _USAGE;									\
+			ast_free(command);									\
+			return NULL;										\
+		} else if (cmd == CLI_GENERATE) {								\
+			for (completer=0; completer<ARRAY_LEN(cli_complete); completer++) {			\
+				if ((unsigned)a->pos == (completer + ARRAY_LEN(cli_command) - 1)) {		\
+					return sccp_exec_completer(cli_complete[completer], (char *)a->line, (char *)a->word, a->pos, a->n);\
+				}										\
+			}											\
+			return NULL;										\
+		}												\
+														\
+		if (a->argc < (int)(ARRAY_LEN(cli_command))) 							\
+			return CLI_SHOWUSAGE;									\
+														\
+		if(_CALLED_FUNCTION(a->fd, a->argc, a->argv) == RESULT_SUCCESS)					\
+			return CLI_SUCCESS;									\
+		else												\
+			return CLI_FAILURE;									\
+	};
+#    define CLI_ENTRY(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE)					\
+	static char *_FUNCTION_NAME(struct ast_cli_entry *e, int cmd, struct ast_cli_args *a) {			\
+		static char *cli_command[] = { CLI_COMMAND };							\
+		char *command=NULL;										\
+		if (cmd == CLI_INIT) {										\
+			if(!implode( cli_command," ", &command)) {						\
+				ast_free(command);								\
+				return CLI_FAILURE;								\
+			}											\
+			e->command = strdup(command);								\
+			e->usage = _USAGE;									\
+			ast_free(command);									\
+		} else if (cmd == CLI_GENERATE)									\
+			return NULL;										\
+														\
+		if (a->argc < (int)(ARRAY_LEN(cli_command)-1))	 						\
+			return CLI_SHOWUSAGE;									\
+														\
+		if(_CALLED_FUNCTION(a->fd, a->argc, a->argv) == RESULT_SUCCESS)					\
+			return CLI_SUCCESS;									\
+		else												\
+			return CLI_FAILURE;									\
 	};
 // END CLI_ENTRY
 
