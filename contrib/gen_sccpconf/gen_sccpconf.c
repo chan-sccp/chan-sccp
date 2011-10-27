ec77b4699f50cd9f9a95d1b9ae355bfbd028e776
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2011-10-27 14:16:25 +0000

    Updated gen_sccpconf (the automatic sccp.conf generator)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@3098 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- contrib/gen_sccpconf/gen_sccpconf.c
+++ contrib/gen_sccpconf/gen_sccpconf.c
@@ -26,7 +26,12 @@
 #include "../../src/config.h"
 #include "gen_sccpconf.h"
 
-static int sccp_config_generate(char *filename, size_t sizeof_filename)  {
+#define CONFIG_TYPE_ALL 0
+#define CONFIG_TYPE_DEFAULTS 1
+#define CONFIG_TYPE_TEMPLATED 2
+#define CONFIG_TYPE_SHORT 3
+
+static int sccp_config_generate(const char *filename, size_t sizeof_filename, int config_type)  {
         const SCCPConfigSegment *sccpConfigSegment = NULL;
         const SCCPConfigOption *config=NULL;
         long unsigned int sccp_option;
@@ -61,7 +66,13 @@ static int sccp_config_generate(char *filename, size_t sizeof_filename)  {
 	for (segment=SCCP_CONFIG_GLOBAL_SEGMENT; segment <= SCCP_CONFIG_SOFTKEY_SEGMENT; segment++) {			
 		sccpConfigSegment = sccp_find_segment(segment);
 		printf("info:" "adding [%s] section\n", sccpConfigSegment->name);
-		fprintf(f, "\n;\n; %s section\n;\n[%s]\n", sccpConfigSegment->name, sccpConfigSegment->name);
+		if (CONFIG_TYPE_TEMPLATED == config_type && SCCP_CONFIG_DEVICE_SEGMENT == segment) {
+			fprintf(f, "\n;\n; %s section\n;\n[%s_template](!)                                                              ; create new template\n", sccpConfigSegment->name, sccpConfigSegment->name);
+		} else if (CONFIG_TYPE_TEMPLATED == config_type && SCCP_CONFIG_LINE_SEGMENT == segment) {
+			fprintf(f, "\n;\n; %s section\n;\n[%s_template](!)                                                                ; create new template\n", sccpConfigSegment->name, sccpConfigSegment->name);
+		} else {
+			fprintf(f, "\n;\n; %s section\n;\n[%s]\n", sccpConfigSegment->name, sccpConfigSegment->name);
+		}
 
 		config = sccpConfigSegment->config;
 		for (sccp_option = 0; sccp_option < sccpConfigSegment->config_size; sccp_option++) {
@@ -69,24 +80,43 @@ static int sccp_config_generate(char *filename, size_t sizeof_filename)  {
 				printf("info:" "adding name: %s, default_value: %s\n", config[sccp_option].name, config[sccp_option].defaultValue);
 				
 				if (config[sccp_option].name && strlen(config[sccp_option].name)!=0) {
-					if ((config[sccp_option].defaultValue && strlen(config[sccp_option].defaultValue)!=0)		// non empty
-						|| ((config[sccp_option].flags & SCCP_CONFIG_FLAG_REQUIRED) != 0 && (config[sccp_option].defaultValue && strlen(config[sccp_option].defaultValue)==0)) // empty but required
-					) {
-						snprintf(name_and_value, sizeof(name_and_value),"%s = %s", config[sccp_option].name, strlen(config[sccp_option].defaultValue)==0 ? "\"\"" : config[sccp_option].defaultValue);
-						linelen=(int)strlen(name_and_value);
-						fprintf(f, "%s", name_and_value);
-						if (config[sccp_option].description && strlen(config[sccp_option].description)!=0) {
-							description=malloc(sizeof(char) * strlen(config[sccp_option].description));	
-							description=strdup(config[sccp_option].description);	
-							while ((description_part=strsep(&description, "\n"))) {
-								if (description_part && strlen(description_part)!=0) {
-									fprintf(f, "%*.s ; %s\n", 81-linelen, " ", description_part);
-									linelen=0;
-								}
-							}	
-						} else {
-							fprintf(f, "\n");
-						}
+					switch (config_type){
+						case CONFIG_TYPE_ALL:
+							break;	
+						case CONFIG_TYPE_DEFAULTS:
+							if ( (((config[sccp_option].flags & SCCP_CONFIG_FLAG_REQUIRED) == 0) && (!config[sccp_option].defaultValue || strlen(config[sccp_option].defaultValue)==0)) ) {
+								continue;
+							}
+							break;	
+						case CONFIG_TYPE_TEMPLATED:
+							if ( (!config[sccp_option].defaultValue || strlen(config[sccp_option].defaultValue)==0) ) {
+								continue;
+							}
+							break;	
+						case CONFIG_TYPE_SHORT:		// SHORT
+							if ( (((config[sccp_option].flags & SCCP_CONFIG_FLAG_REQUIRED) == 0) && (!config[sccp_option].defaultValue || strlen(config[sccp_option].defaultValue)==0)) ) {
+								continue;
+							}
+							break;	
+					}
+					if (config[sccp_option].defaultValue && !strlen(config[sccp_option].defaultValue)==0) {
+						snprintf(name_and_value, sizeof(name_and_value),"%s = %s", config[sccp_option].name, config[sccp_option].defaultValue);
+					} else {
+						snprintf(name_and_value, sizeof(name_and_value),"%s = \"\"", config[sccp_option].name);
+					}							
+					linelen=(int)strlen(name_and_value);
+					fprintf(f, "%s", name_and_value);
+					if (CONFIG_TYPE_SHORT != config_type && config[sccp_option].description && strlen(config[sccp_option].description)!=0) {
+						description=malloc(sizeof(char) * strlen(config[sccp_option].description));	
+						description=strdup(config[sccp_option].description);	
+						while ((description_part=strsep(&description, "\n"))) {
+							if (description_part && strlen(description_part)!=0) {
+								fprintf(f, "%*.s ; %s%s\n", 81-linelen, " ", (config[sccp_option].flags & SCCP_CONFIG_FLAG_REQUIRED) != 0 ? "(REQUIRED) " : "", description_part);
+								linelen=0;
+							}
+						}	
+					} else {
+						fprintf(f, "\n");
 					}
 				} else {
 					printf("error:" "Error creating new variable structure for %s='%s'\n", config[sccp_option].name, config[sccp_option].defaultValue);
@@ -95,6 +125,49 @@ static int sccp_config_generate(char *filename, size_t sizeof_filename)  {
 			}
 		}
 		printf("\n");
+		if (CONFIG_TYPE_TEMPLATED == config_type) {
+			// make up two devices and two lines
+			if (SCCP_CONFIG_DEVICE_SEGMENT == segment) {
+				fprintf(f, "\n");
+				fprintf(f, "[SEP001B535CD5E4](device_template)                                                ; use template 'device_template'\n");
+				fprintf(f, "description=sample1\n");
+				fprintf(f, "devicetype=7960\n");
+				fprintf(f, "button=line, 110\n");
+				fprintf(f, "button=line, 200@01:_SharedLine\n");
+				fprintf(f, "\n");
+				fprintf(f, "[SEP0024C4444763](device_template)                                                ; use template 'device_template'\n");
+				fprintf(f, "description=sample1\n");
+				fprintf(f, "devicetype=7962\n");
+				fprintf(f, "button=line, 111\n");
+				fprintf(f, "button=line, 200@02:_SharedLine\n");
+				fprintf(f, "\n");
+			}
+			if (SCCP_CONFIG_LINE_SEGMENT == segment) {
+				fprintf(f, "\n");
+				fprintf(f, "[110](line_template)                                                              ; use template 'line_template'\n");
+				fprintf(f, "id=110\n");
+				fprintf(f, "label=sample_110\n");
+				fprintf(f, "description=sampleline_110\n");
+				fprintf(f, "cid_num=110\n");
+				fprintf(f, "cid_name=user110\n");
+				fprintf(f, "\n");
+				fprintf(f, "[111](line_template)                                                              ; use template 'line_template'\n");
+				fprintf(f, "id=111\n");
+				fprintf(f, "label=sample_111\n");
+				fprintf(f, "description=sampleline_111\n");
+				fprintf(f, "cid_num=111\n");
+				fprintf(f, "cid_name=user111\n");
+				fprintf(f, "\n");
+				fprintf(f, "[200](line_template)                                                              ; use template 'line_template'\n");
+				fprintf(f, "id=200\n");
+				fprintf(f, "label=sample_share\n");
+				fprintf(f, "description=sharedline\n");
+				fprintf(f, "cid_num=200\n");
+				fprintf(f, "cid_name=shared\n");
+				fprintf(f, "\n");
+			}
+		
+		}
 	}
         fclose(f);
 
@@ -104,19 +177,30 @@ static int sccp_config_generate(char *filename, size_t sizeof_filename)  {
 int main(int argc, char *argv[])
 {
 	char *config_file="";
+	int  config_type=0;
 	
-	if(argc>1) {
+	if(argc>2) {
 		config_file=malloc(sizeof(char*) * strlen(argv[1]) + 1);
 		config_file=strdup(argv[1]);	
+
+		if (!strcasecmp(argv[2], "ALL")) {
+			config_type=CONFIG_TYPE_ALL;
+		}
+		else if (!strcasecmp(argv[2], "DEFAULTS")) {
+			config_type=CONFIG_TYPE_DEFAULTS;
+		}
+		else if (!strcasecmp(argv[2], "TEMPLATED")) {
+			config_type=CONFIG_TYPE_TEMPLATED;
+		}
+		else if (!strcasecmp(argv[2], "SHORT")) {
+			config_type=CONFIG_TYPE_SHORT;	
+		}
 	} else {
-		printf("Usage: gen_sccpconf <config filename>\n");	
+		printf("Usae: gen_sccpconf <config filename> <conf_type>\n");	
+		printf("      where conf_type is one of: ALL | DEFAULTS | TEMPLATED | SHORT\n");
 		return 1;
 	}
 	
-	if (sccp_config_generate(config_file, sizeof(config_file))) {
-		return 0;
-	} else {
-		return 255;
-	}
+	return sccp_config_generate(config_file, sizeof(config_file), config_type);
 }
 
