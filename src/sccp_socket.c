40d0edcfb643cdc22eedad37c0901b6e109a05b7
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2012-04-14 13:49:56 +0000

    Fix sccp_session_send continue
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@3395 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_socket.c
+++ src/sccp_socket.c
@@ -609,16 +609,17 @@ int sccp_session_send2(sccp_session_t * s, sccp_moo_t * r)
 		r->lel_reserved = 0;
 	}
 
-	try = 1;
+	try = 0;
 	maxTries = 50;
 	bytesSent = 0;
 	bufAddr = ((uint8_t *) r);
 	bufLen = (ssize_t) (letohl(r->length) + 8);
 	do {
+		try++;
 		res = write(s->fds[0].fd, bufAddr + bytesSent, bufLen - bytesSent);
                 if (res < 0) { 
                         if (errno != EINTR && errno != EAGAIN) {
-                                ast_log(LOG_WARNING, "%s: write returned %d (error: '%s'). Sent %d of %d for Message: '%s' with total length %d \n", DEV_ID_LOG(s->device), (int)res, strerror(errno), (int)bytesSent, (int)bufLen, message2str(letohl(r->lel_messageId)), letohl(r->length));
+                                pbx_log(LOG_WARNING, "%s: write returned %d (error: '%s'). Sent %d of %d for Message: '%s' with total length %d \n", DEV_ID_LOG(s->device), (int)res, strerror(errno), (int)bytesSent, (int)bufLen, message2str(letohl(r->lel_messageId)), letohl(r->length));
                                 sccp_dump_packet((unsigned char *)&r->msg, (r->length < SCCP_MAX_PACKET)?r->length:SCCP_MAX_PACKET);
                                 if (s) {
                                         ast_log(LOG_WARNING, "%s. closing connection", DEV_ID_LOG(s->device));
@@ -630,10 +631,11 @@ int sccp_session_send2(sccp_session_t * s, sccp_moo_t * r)
                                 }
                                 break;
                         } 
+                        sccp_log (DEBUGCAT_HIGH)(VERBOSE_PREFIX_4 "%s: EAGAIN / EINTR: %d (error: '%s'). Sent %d of %d for Message: '%s' with total length %d  \n", DEV_ID_LOG(s->device), (int)res, strerror(errno), (int)bytesSent, (int)bufLen, message2str(letohl(r->lel_messageId)), letohl(r->length));
                         usleep(50);		// try sending more data
+                        continue; 
                 }
 		bytesSent += res;
-		try++;
 	} while (bytesSent < bufLen && try < maxTries && s && !s->session_stop && s->fds[0].fd > 0);
 
 	sccp_session_unlock(s);
