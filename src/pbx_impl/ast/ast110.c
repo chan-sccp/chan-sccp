ebda12a2b12ee40df0863887a004b9dc850700e5
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2012-04-14 09:17:03 +0000

    Fix bad magic number on hangup (Reported by Diego/Philippe)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@3382 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast110.c
+++ src/pbx_impl/ast/ast110.c
@@ -749,8 +749,11 @@ int sccp_wrapper_asterisk110_hangup(PBX_CHANNEL_TYPE * ast_channel)
 	sccp_channel_unlock(c);
 
 	ast_channel->tech_pvt = NULL;
-	ast_channel_unref(ast_channel);
-
+//	ast_channel_unref(ast_channel);
+#if CS_AST_MODULE_REF
+        ast_module_unref(ast_module_info->self);
+#endif
+        
 	return res;
 }
 
