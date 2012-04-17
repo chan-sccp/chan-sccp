bab108924c8692f1f7841cf6b181e61b8fc4fb77
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2012-04-17 09:07:32 +0000

    fixes hangup issue
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@3406 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_pbx.c
+++ src/sccp_pbx.c
@@ -363,9 +363,10 @@ int sccp_pbx_hangup_locked(sccp_channel_t * c)
 	}
 
 	d = sccp_channel_getDevice(c);
-	if (c->state != SCCP_CHANNELSTATE_DOWN && SKINNY_DEVICE_RS_OK == d->registrationState) {
-		if (GLOB(remotehangup_tone) && d && d->state == SCCP_DEVICESTATE_OFFHOOK && c == sccp_channel_get_active_nolock(d))
+	if (c->state != SCCP_CHANNELSTATE_CONNECTED && d && SKINNY_DEVICE_RS_OK == d->registrationState) {
+		if (GLOB(remotehangup_tone) && d && d->state == SCCP_DEVICESTATE_OFFHOOK && c == sccp_channel_get_active_nolock(d)){
 			sccp_dev_starttone(d, GLOB(remotehangup_tone), 0, 0, 10);
+		}
 		sccp_indicate_locked(d, c, SCCP_CHANNELSTATE_ONHOOK);
 	}
 
