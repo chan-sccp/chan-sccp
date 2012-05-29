e38bdd9b94788b389debd6c69f80f4723d9cdc8c
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2012-05-29 17:30:21 +0000

    add missing files
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@3473 43c1dc8c-776e-4df2-8ef1-6e829299be21

---
+++ src/sccp_threadpool.c
@@ -0,0 +1,395 @@
+
+/*!
+ * \file 	sccp_threadpool.c
+ * \brief 	SCCP Threadpool Class
+ * \author 	Diederik de Groot < ddegroot@users.sourceforge.net >
+ * \note	This program is free software and may be modified and distributed under the terms of the GNU Public License.
+ *		See the LICENSE file at the top of the source tree.
+ * \note	Based on the work of Johan Hanssen Seferidis
+ * 		Library providing a threading pool where you can add work. 
+ * \since 	2009-01-16
+ * \remarks	Purpose: 	SCCP Hint
+ * 		When to use:	Does the business of hint status
+ *
+ * $Date: 2011-01-04 17:29:12 +0100 (Tue, 04 Jan 2011) $
+ * $Revision: 2215 $
+ */
+
+#include "config.h"
+#include "common.h"
+
+SCCP_FILE_VERSION(__FILE__, "$Revision: 2215 $")
+#include <semaphore.h>
+#include "sccp_threadpool.h"
+
+/* The threadpool */
+struct sccp_threadpool_t {
+	pthread_t *threads;							/*!< pointer to threads' ID   */
+	int threadsN;								/*!< amount of threads        */
+	sccp_threadpool_jobqueue *jobqueue;					/*!< pointer to the job queue */
+	time_t last_size_check;							/*!< Time since last resize check */
+	int job_high_water_mark;						/*!< Highest number of jobs outstanding since last resize check */
+};
+
+/* Container for all things that each thread is going to need */
+struct thread_data {
+	sccp_mutex_t *mutex_p;
+	sccp_threadpool_t *tp_p;
+};
+
+/* 
+ * Fast reminders:
+ * 
+ * tp 	         	= threadpool 
+ * sccp_threadpool      = threadpool
+ * sccp_threadpool_t    = threadpool type
+ * tp_p         	= threadpool pointer
+ * sem  	        = semaphore
+ * xN           	= x can be any string. N stands for amount
+ * */
+static int sccp_threadpool_keepalive = 1;
+static int sccp_threadpool_shuttingdown = 0;
+
+/* Create mutex variable */
+AST_MUTEX_DEFINE_STATIC(threadpool_mutex);									/* used to serialize queue access */
+
+/* Initialise thread pool */
+sccp_threadpool_t *sccp_threadpool_init(int threadsN)
+{
+#ifndef CPU_COUNT
+	#warning CPU_COUNT not defined
+#endif	
+	sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Starting Threadpool\n");
+	sccp_threadpool_t *tp_p;
+
+	if (!threadsN || threadsN < THREADPOOL_MIN_SIZE)
+		threadsN = THREADPOOL_MIN_SIZE;
+	
+	if (threadsN > THREADPOOL_MAX_SIZE)
+		threadsN = THREADPOOL_MAX_SIZE;
+
+	/* Make new thread pool */
+	tp_p = (sccp_threadpool_t *) malloc(sizeof(sccp_threadpool_t));						/* MALLOC thread pool */
+	if (tp_p == NULL) {
+		pbx_log(LOG_ERROR, "sccp_threadpool_init(): Could not allocate memory for thread pool\n");
+		return NULL;
+	}
+	tp_p->threads = (pthread_t *) malloc(threadsN * sizeof(pthread_t));					/* MALLOC thread IDs */
+	if (tp_p->threads == NULL) {
+		pbx_log(LOG_ERROR, "sccp_threadpool_init(): Could not allocate memory for thread IDs\n");
+		return NULL;
+	}
+	tp_p->threadsN = threadsN;
+
+	/* Initialise the job queue */
+	if (sccp_threadpool_jobqueue_init(tp_p) == -1) {
+		pbx_log(LOG_ERROR, "sccp_threadpool_init(): Could not allocate memory for job queue\n");
+		return NULL;
+	}
+
+	/* Initialise semaphore */
+	tp_p->jobqueue->queueSem = (sem_t *) malloc(sizeof(sem_t));						/* MALLOC job queue semaphore */
+	sem_init(tp_p->jobqueue->queueSem, 0, 0);								/* no shared, initial value */
+
+	/* Make threads in pool */
+	int t;
+	
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+
+	for (t = 0; t < threadsN; t++) {
+		sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Create thread %d in pool \n", t);
+		
+		pbx_pthread_create(&(tp_p->threads[t]), &attr, (void *)sccp_threadpool_thread_do, (void *)tp_p);	/* MALLOCS INSIDE PTHREAD HERE */
+	}
+
+	sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Threadpool Started\n");
+	return tp_p;
+}
+
+/* resize threadpool */
+static void sccp_thread_resize(sccp_threadpool_t * tp_p, int threadsN)
+{
+	sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Resizing threadpool to %d threads\n", threadsN);
+	void *tmpPtr = realloc(tp_p, threadsN * sizeof(pthread_t));		/* REALLOC thread IDs */
+	if (tmpPtr == NULL) {
+		pbx_log(LOG_ERROR, "sccp_threadpool_init(): Could not allocate memory for thread IDs\n");
+		return;
+	}
+	tp_p->threads = (pthread_t *) tmpPtr;
+	tp_p->threadsN = threadsN;
+}
+
+/* check threadpool size (increase/decrease if necessary)*/
+static void sccp_threadpool_check_size(sccp_threadpool_t * tp_p)
+{
+	int t;
+	int newsize;
+
+	if (tp_p->jobqueue->jobsN > (tp_p->threadsN * 2) && tp_p->threadsN < THREADPOOL_MAX_SIZE) {		/* increase */
+		sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Add new thread to threadpool %p\n", tp_p);
+
+		newsize = tp_p->threadsN + 1;
+		sccp_thread_resize(tp_p, newsize);
+		pthread_create(&(tp_p->threads[tp_p->threadsN - 1]), NULL, (void *)sccp_threadpool_thread_do, (void *)tp_p);
+
+	} else if (tp_p->threadsN > THREADPOOL_MIN_SIZE && tp_p->jobqueue->jobsN > (tp_p->threadsN / 2)) {	/* increase */
+		newsize = tp_p->threadsN - 1;
+		sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Remove thread %d from threadpool %p\n", newsize, tp_p);
+		/* Send thread cancel message */
+		pthread_cancel(tp_p->threads[newsize - 1]);
+		/* Wake up all threads */
+		for (t = 0; t < (tp_p->threadsN); t++) {
+			if (sem_post(tp_p->jobqueue->queueSem)) {
+				pbx_log(LOG_ERROR, "sccp_threadpool_destroy(): Could not bypass sem_wait()\n");
+			}
+		}
+		/* Wait for threads to finish */
+		pthread_join(tp_p->threads[newsize - 1], NULL);
+		sccp_thread_resize(tp_p, newsize);
+	}
+	tp_p->last_size_check = time(0);
+	tp_p->job_high_water_mark = tp_p->jobqueue->jobsN;
+}
+
+/* What each individual thread is doing 
+ * */
+
+/* There are two scenarios here. One is everything works as it should and second if
+ * the sccp_threadpool is to be killed. In that manner we try to BYPASS sem_wait and end each thread. */
+void sccp_threadpool_thread_do(sccp_threadpool_t * tp_p)
+{
+	while (sccp_threadpool_keepalive) {
+		pthread_testcancel();
+		if (tp_p && tp_p->jobqueue && sem_wait(tp_p->jobqueue->queueSem)) {							/* WAITING until there is work in the queue */
+			pbx_log(LOG_ERROR, "sccp_threadpool_thread_do(): Error while waiting for semaphore");
+			exit(1);
+		}
+		if (sccp_threadpool_keepalive) {
+			/* Read job from queue and execute it */
+			void *(*func_buff) (void *arg);
+			void *arg_buff;
+			sccp_threadpool_job_t *job_p;
+
+			pbx_mutex_lock(&threadpool_mutex);							/* LOCK */
+
+			job_p = sccp_threadpool_jobqueue_peek(tp_p);
+			func_buff = job_p->function;
+			arg_buff = job_p->arg;
+			sccp_threadpool_jobqueue_removelast(tp_p);
+
+			pbx_mutex_unlock(&threadpool_mutex);							/* UNLOCK */
+
+			func_buff(arg_buff);									/* run function */
+			free(job_p);										/* DEALLOC job */
+		} else {
+			return;											/* EXIT thread */
+		}
+
+		if ((time(0) - tp_p->last_size_check) > THREADPOOL_RESIZE_INTERVAL) {
+			pbx_mutex_lock(&threadpool_mutex);							/* LOCK */
+			sccp_threadpool_check_size(tp_p);
+			pbx_mutex_unlock(&threadpool_mutex);							/* UNLOCK */
+		}
+	}
+	return;
+}
+
+/* Add work to the thread pool */
+int sccp_threadpool_add_work(sccp_threadpool_t * tp_p, void *(*function_p) (void *), void *arg_p)
+{
+	// prevent new work while shutting down
+//	if (GLOB(module_running)) {
+	if (!sccp_threadpool_shuttingdown) {
+		sccp_threadpool_job_t *newJob;
+
+		newJob = (sccp_threadpool_job_t *) malloc(sizeof(sccp_threadpool_job_t));				/* MALLOC job */
+		if (newJob == NULL) {
+			pbx_log(LOG_ERROR, "sccp_threadpool_add_work(): Could not allocate memory for new job\n");
+			exit(1);
+		}
+
+		/* add function and argument */
+		newJob->function = function_p;
+		newJob->arg = arg_p;
+
+		/* add job to queue */
+		sccp_threadpool_jobqueue_add(tp_p, newJob);
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+/* Destroy the threadpool */
+void sccp_threadpool_destroy(sccp_threadpool_t * tp_p)
+{
+	int t;
+	sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Destroying Threadpool %p with %d jobs\n", tp_p, tp_p->jobqueue->jobsN);
+
+
+	sccp_threadpool_shuttingdown = 1;
+	// wake up jobs untill jobqueue is empty, before shutting down, to make sure all jobs have been processed
+	while (tp_p->jobqueue->jobsN > 0) {
+		for (t = 0; t < (tp_p->threadsN); t++) {
+			if (sem_post(tp_p->jobqueue->queueSem)) {
+				pbx_log(LOG_ERROR, "sccp_threadpool_destroy(): Could not bypass sem_wait()\n");
+			}
+		}
+	}
+
+	/* End each thread's infinite loop */
+	sccp_threadpool_keepalive = 0;
+
+	/* Awake idle threads waiting at semaphore */
+	for (t = 0; t < (tp_p->threadsN); t++) {
+		if (sem_post(tp_p->jobqueue->queueSem)) {
+			pbx_log(LOG_ERROR, "sccp_threadpool_destroy(): Could not bypass sem_wait()\n");
+		}
+	}
+
+	/* Kill semaphore */
+	if (sem_destroy(tp_p->jobqueue->queueSem) != 0) {
+		pbx_log(LOG_ERROR, "sccp_threadpool_destroy(): Could not destroy semaphore\n");
+	}
+
+	/* Wait for threads to finish */
+	for (t = 0; t < (tp_p->threadsN); t++) {
+		pthread_cancel(tp_p->threads[t]);
+		pthread_kill(tp_p->threads[t], SIGURG);
+		pthread_join(tp_p->threads[t], NULL);
+	}
+
+	sccp_threadpool_jobqueue_empty(tp_p);
+
+	/* Dealloc */
+	free(tp_p->threads);											/* DEALLOC threads             */
+	free(tp_p->jobqueue->queueSem);										/* DEALLOC job queue semaphore */
+	free(tp_p->jobqueue);											/* DEALLOC job queue           */
+	free(tp_p);												/* DEALLOC thread pool         */
+	sccp_log(DEBUGCAT_CORE) (VERBOSE_PREFIX_3 "Threadpool Ended\n");
+}
+
+int sccp_threadpool_thread_count(sccp_threadpool_t * tp_p)
+{
+	return tp_p->threadsN;
+}
+/* =================== JOB QUEUE OPERATIONS ===================== */
+
+/* Initialise queue */
+int sccp_threadpool_jobqueue_init(sccp_threadpool_t * tp_p)
+{
+	tp_p->jobqueue = (sccp_threadpool_jobqueue *) malloc(sizeof(sccp_threadpool_jobqueue));			/* MALLOC job queue */
+	if (tp_p->jobqueue == NULL)
+		return -1;
+	tp_p->jobqueue->tail = NULL;
+	tp_p->jobqueue->head = NULL;
+	tp_p->jobqueue->jobsN = 0;
+	tp_p->last_size_check = time(0);
+	tp_p->job_high_water_mark = 0;
+	return 0;
+}
+
+/* Add job to queue */
+void sccp_threadpool_jobqueue_add(sccp_threadpool_t * tp_p, sccp_threadpool_job_t * newjob_p)
+{														/* remember that job prev and next point to NULL */
+
+	newjob_p->next = NULL;
+	newjob_p->prev = NULL;
+
+	sccp_threadpool_job_t *oldFirstJob;
+
+	oldFirstJob = tp_p->jobqueue->head;
+
+	/* fix jobs' pointers */
+	switch (tp_p->jobqueue->jobsN) {
+
+	case 0:													/* if there are no jobs in queue */
+		tp_p->jobqueue->tail = newjob_p;
+		tp_p->jobqueue->head = newjob_p;
+		break;
+
+	default:												/* if there are already jobs in queue */
+		oldFirstJob->prev = newjob_p;
+		newjob_p->next = oldFirstJob;
+		tp_p->jobqueue->head = newjob_p;
+
+	}
+
+	(tp_p->jobqueue->jobsN)++;										/* increment amount of jobs in queue */
+	if (tp_p->jobqueue->jobsN > tp_p->job_high_water_mark)
+		tp_p->job_high_water_mark = tp_p->jobqueue->jobsN;
+		
+	sem_post(tp_p->jobqueue->queueSem);
+
+	int sval;
+
+	sem_getvalue(tp_p->jobqueue->queueSem, &sval);
+}
+
+/* Remove job from queue */
+int sccp_threadpool_jobqueue_removelast(sccp_threadpool_t * tp_p)
+{
+	sccp_threadpool_job_t *oldLastJob;
+
+	oldLastJob = tp_p->jobqueue->tail;
+
+	/* fix jobs' pointers */
+	switch (tp_p->jobqueue->jobsN) {
+
+	case 0:													/* if there are no jobs in queue */
+		return -1;
+		break;
+
+	case 1:													/* if there is only one job in queue */
+		tp_p->jobqueue->tail = NULL;
+		tp_p->jobqueue->head = NULL;
+		break;
+
+	default:												/* if there are more than one jobs in queue */
+		oldLastJob->prev->next = NULL;									/* the almost last item */
+		tp_p->jobqueue->tail = oldLastJob->prev;
+
+	}
+
+	(tp_p->jobqueue->jobsN)--;
+
+	int sval;
+
+	sem_getvalue(tp_p->jobqueue->queueSem, &sval);
+	return 0;
+}
+
+/* Get first element from queue */
+sccp_threadpool_job_t *sccp_threadpool_jobqueue_peek(sccp_threadpool_t * tp_p)
+{
+	return tp_p->jobqueue->tail;
+}
+
+/* Remove and deallocate all jobs in queue */
+void sccp_threadpool_jobqueue_empty(sccp_threadpool_t * tp_p)
+{
+
+	sccp_threadpool_job_t *curjob;
+
+	curjob = tp_p->jobqueue->tail;
+
+	while (tp_p->jobqueue->jobsN) {
+		tp_p->jobqueue->tail = curjob->prev;
+		free(curjob);
+		curjob = tp_p->jobqueue->tail;
+		tp_p->jobqueue->jobsN--;
+	}
+
+	/* Fix head and tail */
+	tp_p->jobqueue->tail = NULL;
+	tp_p->jobqueue->head = NULL;
+}
+
+int sccp_threadpool_jobqueue_count(sccp_threadpool_t * tp_p) 
+{
+	return tp_p->jobqueue->jobsN;
+}
---
+++ src/sccp_threadpool.h
@@ -0,0 +1,220 @@
+
+/*!
+ * \file 	sccp_threadpool.h
+ * \brief 	SCCP Threadpool Header
+ * \author 	Diederik de Groot < ddegroot@users.sourceforge.net >
+ * \note        This program is free software and may be modified and distributed under the terms of the GNU Public License.
+ *		See the LICENSE file at the top of the source tree.
+ * \note	Based on the work of Johan Hanssen Seferidis
+ * 		Library providing a threading pool where you can add work. 
+ * \since 	2009-01-16
+ *
+ * $Date: 2010-11-17 12:03:44 +0100 (Wed, 17 Nov 2010) $
+ * $Revision: 2130 $  
+ */
+#ifndef SCCP_THREADPOOL_H_
+#    define SCCP_THREADPOOL_H_
+
+//#include "config.h"
+//#    include <asterisk.h>
+//#    include <asterisk/lock.h>
+#    include <pthread.h>
+#    include <semaphore.h>
+
+/* Description: Library providing a threading pool where you can add work on the fly. The number
+ *              of threads in the pool is adjustable when creating the pool. In most cases
+ *              this should equal the number of threads supported by your cpu.
+ *          
+ *              In this header file a detailed overview of the functions and the threadpool logical
+ *              scheme is present in case tweaking of the pool is needed. 
+ * */
+
+/* 
+ * Fast reminders:
+ * 
+ * tp           = threadpool 
+ * sccp_threadpool       = threadpool
+ * sccp_threadpool_t     = threadpool type
+ * tp_p         = threadpool pointer
+ * sem          = semaphore
+ * xN           = x can be any string. N stands for amount
+ * 
+ * */
+
+/*              _______________________________________________________        
+ *             /                                                       \
+ *             |   JOB QUEUE        | job1 | job2 | job3 | job4 | ..   |
+ *             |                                                       |
+ *             |   threadpool      | thread1 | thread2 | ..           |
+ *             \_______________________________________________________/
+ * 
+ * Description:       Jobs are added to the job queue. Once a thread in the pool
+ *                    is idle, it is assigned with the first job from the queue(and
+ *                    erased from the queue). It's each thread's job to read from 
+ *                    the queue serially(using lock) and executing each job
+ *                    until the queue is empty.
+ * 
+ * 
+ *    Scheme:
+ * 
+ *    sccp_threadpool______                jobqueue____                      ______ 
+ *    |           |               |           |       .----------->|_job0_| Newly added job
+ *    |           |               |  head------------'             |_job1_|
+ *    | jobqueue----------------->|           |                    |_job2_|
+ *    |           |               |  tail------------.             |__..__| 
+ *    |___________|               |___________|       '----------->|_jobn_| Job for thread to take
+ * 
+ * 
+ *    job0________ 
+ *    |           |
+ *    | function---->
+ *    |           |
+ *    |   arg------->
+ *    |           |         job1________ 
+ *    |  next-------------->|           |
+ *    |___________|         |           |..
+ */
+
+/* ================================= STRUCTURES ================================================ */
+
+/* Individual job */
+typedef struct sccp_threadpool_job_t {
+	void *(*function) (void *arg);						/*!< function pointer         */
+	void *arg;								/*!< function's argument      */
+	struct sccp_threadpool_job_t *next;					/*!< pointer to next job      */
+	struct sccp_threadpool_job_t *prev;					/*!< pointer to previous job  */
+} sccp_threadpool_job_t;
+
+/* Job queue as doubly linked list */
+typedef struct sccp_threadpool_jobqueue {
+	sccp_threadpool_job_t *head;						/*!< pointer to head of queue */
+	sccp_threadpool_job_t *tail;						/*!< pointer to tail of queue */
+	int jobsN;								/*!< amount of jobs in queue  */
+	sem_t *queueSem;							/*!< semaphore(this is probably just holding the same as jobsN) */
+} sccp_threadpool_jobqueue;
+
+typedef struct sccp_threadpool_t sccp_threadpool_t;
+typedef struct thread_data thread_data;
+
+/* =========================== FUNCTIONS ================================================ */
+
+/* ----------------------- Threadpool specific --------------------------- */
+
+/*!
+ * \brief  Initialize threadpool
+ * 
+ * Allocates memory for the threadpool, jobqueue, semaphore and fixes 
+ * pointers in jobqueue.
+ * 
+ * \param  number of threads to be used
+ * \return threadpool struct on success,
+ *         NULL on error
+ */
+sccp_threadpool_t *sccp_threadpool_init(int threadsN);
+
+/*!
+ * \brief What each thread is doing
+ * 
+ * In principle this is an endless loop. The only time this loop gets interuppted is once
+ * sccp_threadpool_destroy() is invoked.
+ * 
+ * \param threadpool to use
+ * \return nothing
+ */
+void sccp_threadpool_thread_do(sccp_threadpool_t * tp_p);
+
+/*!
+ * \brief Add work to the job queue
+ * 
+ * Takes an action and its argument and adds it to the threadpool's job queue.
+ * If you want to add to work a function with more than one arguments then
+ * a way to implement this is by passing a pointer to a structure.
+ * 
+ * ATTENTION: You have to cast both the function and argument to not get warnings.
+ * 
+ * \param  threadpool to where the work will be added to
+ * \param  function to add as work
+ * \param  argument to the above function
+ * \return int
+ */
+int sccp_threadpool_add_work(sccp_threadpool_t * tp_p, void *(*function_p) (void *), void *arg_p);
+
+/*!
+ * \brief Destroy the threadpool
+ * 
+ * This will 'kill' the threadpool and free up memory. If threads are active when this
+ * is called, they will finish what they are doing and then they will get destroyied.
+ * 
+ * \param threadpool a pointer to the threadpool structure you want to destroy
+ */
+void sccp_threadpool_destroy(sccp_threadpool_t * tp_p);
+
+/*!
+ * \brief Return number of currently allocate threads in the threadpool
+ */
+int sccp_threadpool_thread_count(sccp_threadpool_t * tp_p);
+
+/* ------------------------- Queue specific ------------------------------ */
+
+/*!
+ * \brief Initialize queue
+ * \param  pointer to threadpool
+ * \return 0 on success,
+ *        -1 on memory allocation error
+ */
+int sccp_threadpool_jobqueue_init(sccp_threadpool_t * tp_p);
+
+/*!
+ * \brief Add job to queue
+ * 
+ * A new job will be added to the queue. The new job MUST be allocated
+ * before passed to this function or else other functions like sccp_threadpool_jobqueue_empty()
+ * will be broken.
+ * 
+ * \param pointer to threadpool
+ * \param pointer to the new job(MUST BE ALLOCATED)
+ * \return nothing 
+ */
+void sccp_threadpool_jobqueue_add(sccp_threadpool_t * tp_p, sccp_threadpool_job_t * newjob_p);
+
+/*!
+ * \brief Remove last job from queue. 
+ * 
+ * This does not free allocated memory so be sure to have peeked()
+ * before invoking this as else there will result lost memory pointers.
+ * 
+ * \param  pointer to threadpool
+ * \return 0 on success,
+ *         -1 if queue is empty
+ */
+int sccp_threadpool_jobqueue_removelast(sccp_threadpool_t * tp_p);
+
+/*! 
+ * \brief Get last job in queue (tail)
+ * 
+ * Gets the last job that is inside the queue. This will work even if the queue
+ * is empty.
+ * 
+ * \param  pointer to threadpool structure
+ * \return job a pointer to the last job in queue,
+ *         a pointer to NULL if the queue is empty
+ */
+sccp_threadpool_job_t *sccp_threadpool_jobqueue_peek(sccp_threadpool_t * tp_p);
+
+/*!
+ * \brief Remove and deallocate all jobs in queue
+ * 
+ * This function will deallocate all jobs in the queue and set the
+ * jobqueue to its initialization values, thus tail and head pointing
+ * to NULL and amount of jobs equal to 0.
+ * 
+ * \param pointer to threadpool structure
+ * */
+void sccp_threadpool_jobqueue_empty(sccp_threadpool_t * tp_p);
+
+/*!
+ * \brief Return Number of Jobs in the Queue
+ */
+int sccp_threadpool_jobqueue_count(sccp_threadpool_t * tp_p);
+
+#endif
