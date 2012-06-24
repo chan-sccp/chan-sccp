21bd6ba8989bc80c5bb7acdb7c898e26936641cf
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2012-06-24 18:40:20 +0000

    check ss_action for Dial softkey
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@3505 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_softkeys.c
+++ src/sccp_softkeys.c
@@ -181,7 +181,8 @@ void sccp_sk_dial(sccp_device_t * d, sccp_line_t * l, const uint32_t lineInstanc
 	if (c) {								// Handle termination of dialling if in appropriate state.
 		/* Only handle this in DIALING state. AFAIK GETDIGITS is used only for call forward and related input functions. (-DD) */
 		if(c->ss_action == SCCP_SS_GETFORWARDEXTEN) {
-			sccp_line_cfwd(c->line, d, c->ss_data, c->dialedNumber);
+			c->scheduler.digittimeout = SCCP_SCHED_DEL(c->scheduler.digittimeout);
+			sccp_pbx_softswitch(c);
 			
 		} else if (c->state == SCCP_CHANNELSTATE_DIALING) {
 			c->scheduler.digittimeout = SCCP_SCHED_DEL(c->scheduler.digittimeout);
