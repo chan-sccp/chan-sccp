39c889daf320c9c59491062b778bb8d088751fbe
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2012-07-06 08:38:10 +0000

    First introduction of 'with_ref' operator
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@3600 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_refcount.h
+++ src/sccp_refcount.h
@@ -12,6 +12,8 @@
 #ifndef __SCCP_REFCOUNT_H
 #    define __SCCP_REFCOUNT_H
 
+#include <setjmp.h>
+
 typedef struct refcount_object RefCountedObject;
 
 void sccp_refcount_init(void);
@@ -22,4 +24,25 @@ void *sccp_refcount_object_alloc(size_t size, const char *datatype, const char *
 void sccp_refcount_updateIdentifier(void *ptr, char *identifier);
 inline void *sccp_refcount_retain(void *ptr, const char *filename, int lineno, const char *func);
 inline void *sccp_refcount_release(const void *ptr, const char *filename, int lineno, const char *func);
+
+/* *INDENT-OFF* */
+#define __WITH_REF(_a,_b,_c,_d) 										\
+  int with_counter_##_a##_c=3;										\
+  while (with_counter_##_a##_c-- > 0)									\
+          if (2 == with_counter_##_a##_c) {		/* ENTRY */					\
+                  if (!(_a = sccp_refcount_retain(_a,_b,_c,_d))) {					\
+                          pbx_log(LOG_NOTICE, "[%s:%d] %s: Failed to retain (%p)\n",  _b,_c,_d,_a);	\
+                          with_counter_##_a##_c=0;							\
+                          break;									\
+                  }											\
+          } else											\
+          if (1 == with_counter_##_a##_c) {		/* EXIT */					\
+                  if ((_a = sccp_refcount_release(_a,_b,_c,_d)) != NULL) {				\
+                          pbx_log(LOG_NOTICE, "[%s:%d] %s: Failed to release (%p)\n", _b,_c,_d,_a);	\
+                  }											\
+                  break;										\
+          } else          				/* DO */
+
+#define with_ref(_a) __WITH_REF(_a,__FILE__,__LINE__,__PRETTY_FUNCTION__)
+/* *INDENT-ON* */
 #endif										// __SCCP_REFCOUNT_H
