4a9544586d0fcf64b19a7dcd360a525699232470
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-01-01 20:06:24 +0000

    Fixed privacy/presentation for sccp_hint.c
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4011 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_softkeys.c
+++ src/sccp_softkeys.c
@@ -851,6 +851,7 @@ void sccp_sk_private(sccp_device_t * d, sccp_line_t * l, const uint32_t lineInst
 //      sccp_feat_changed(d, SCCP_FEATURE_PRIVACY);
 	if (c->privacy) {
 		sccp_dev_displayprompt(d, lineInstance, c->callid, SKINNY_DISP_PRIVATE, 0);
+		c->callInfo.presentation = 0;
 	} else {
 		sccp_dev_displayprompt(d, lineInstance, c->callid, SKINNY_DISP_ENTER_NUMBER, 1);
 	}
