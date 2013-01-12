a13e84074e3d52087380cf9af0965b2c54ca9390
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-01-12 10:32:44 +0000

    Fix rtp audio during grouppickup for asterisk-trunk (2)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4101 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_features.c
+++ src/sccp_features.c
@@ -490,6 +490,7 @@ int sccp_feat_grouppickup(sccp_line_t * l, sccp_device_t * d)
 		if ((c = sccp_channel_allocate(l, d))) {
 			c = sccp_channel_retain(c);
 			PBX(alloc_pbxChannel) (c, &target);
+//			c->previousChannelState=SCCP_CHANNELSTATE_DOWN;
 		} else {
 			pbx_log(LOG_ERROR, "%s: (grouppickup) Can't allocate SCCP channel for line %s\n", d->id, l->name);
 			c = sccp_channel_release(c);
@@ -499,15 +500,15 @@ int sccp_feat_grouppickup(sccp_line_t * l, sccp_device_t * d)
 		target = c->owner;
 	}
 	c->calltype = SKINNY_CALLTYPE_INBOUND;
-	d->state = SCCP_CHANNELSTATE_OFFHOOK;
 	c = ast_channel_tech_pvt(target);
 	res = pbx_pickup_call(target);
 	if (!res) {
 		sccp_log(DEBUGCAT_FEATURE) (VERBOSE_PREFIX_3 "SCCP: (grouppickup) pickup succeeded on callid: %d\n", c->callid);
 		pbx_hangup(target);
-		sccp_indicate(d, c, SCCP_CHANNELSTATE_OFFHOOK);
+//		sccp_indicate(d, c, SCCP_CHANNELSTATE_OFFHOOK);
 		c->rtp.audio.rtp = NULL;							// somehow it is believed we already have c->rtp.audio.rtp ->causing segfault
 		sccp_channel_answer(d, c);
+//		d->state = SCCP_DEVICESTATE_OFFHOOK;
 	} else {
 		sccp_log(DEBUGCAT_FEATURE) (VERBOSE_PREFIX_3 "SCCP: (grouppickup) pickup failed (someone else might have picked it up already)\n");
 		sccp_dev_displayprompt(d, 1, 0, "pickup failed", 5);
