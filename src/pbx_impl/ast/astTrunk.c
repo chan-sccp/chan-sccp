966a52e040c0ab62e38a0ec4770875795f93a6cd
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-02-04 11:09:53 +0000

    Fixed spelling mistake in function name
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4181 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/astTrunk.c
+++ src/pbx_impl/ast/astTrunk.c
@@ -455,7 +455,7 @@ static void sccp_wrapper_asterisk111_connectedline(sccp_channel_t * channel, con
 
 
 
-static void sccp_wrapper_asterisk111_redeirectedUpdate(sccp_channel_t * channel, const void *data, size_t datalen)
+static void sccp_wrapper_asterisk111_redirectedUpdate(sccp_channel_t * channel, const void *data, size_t datalen)
 {
 	PBX_CHANNEL_TYPE *ast = channel->owner;
 
@@ -645,7 +645,7 @@ static int sccp_wrapper_asterisk111_indicate(PBX_CHANNEL_TYPE * ast, int ind, co
 			break;
 		
 		case AST_CONTROL_REDIRECTING:
-			sccp_wrapper_asterisk111_redeirectedUpdate(c, data, datalen);
+			sccp_wrapper_asterisk111_redirectedUpdate(c, data, datalen);
 			sccp_indicate(d, c, c->state);
 
 			res = 0;
