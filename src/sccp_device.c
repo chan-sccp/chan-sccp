fdd7553ad6be5f1bf8d660983053d93f231a1185
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-03-17 12:37:29 +0000

    default disable join button
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4397 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_device.c
+++ src/sccp_device.c
@@ -300,9 +300,10 @@ sccp_device_t *sccp_device_create(const char *id)
 	sccp_mutex_unlock(&d->messageStackLock);
 #endif
 
-	/* disable videomode softkey for all softkeysets */
+	/* disable videomode and join softkey for all softkeysets */
 	for (i = 0; i < KEYMODE_ONHOOKSTEALABLE; i++) {
 		sccp_softkey_setSoftkeyState(d, i, SKINNY_LBL_VIDEO_MODE, FALSE);
+		sccp_softkey_setSoftkeyState(d, i, SKINNY_LBL_JOIN, FALSE);
 	}
 
 	d->pushURL = sccp_device_pushURLNotSupported;
