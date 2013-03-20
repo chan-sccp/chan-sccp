a6cb007dad6285fb1fc19e35737b3a0c3309bcfa
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-03-20 18:19:12 +0000

    fixes openreceive channel after call timeout and asterisk prod
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4437 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast110.c
+++ src/pbx_impl/ast/ast110.c
@@ -700,15 +700,6 @@ static int sccp_wrapper_asterisk110_indicate(PBX_CHANNEL_TYPE * ast, int ind, co
 			break;
 #endif
 		case -1:											// Asterisk prod the channel
-
-		/** this is a dirty workaround to fix audio issue while pickup a parked call
-		 * reason: asterisk do not indicate connected if we dial to a parked extension
-		 * -MC
-		 */
-			if (c->state != SCCP_CHANNELSTATE_CONNECTED) {
-				sccp_log((DEBUGCAT_PBX | DEBUGCAT_INDICATE)) (VERBOSE_PREFIX_3 "SCCP: force CONNECT\n");
-				sccp_indicate(d, c, SCCP_CHANNELSTATE_CONNECTED);
-			}
 			res = -1;
 			break;
 		default:
