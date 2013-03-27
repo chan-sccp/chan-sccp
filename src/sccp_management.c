129e872ec03cbd5da4928a82c6905bf9e76cf789
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-03-27 06:44:42 +0000

    fixes continuous cores on register, when device uses button=feature,x,cfwdall; fixes #97;
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4497 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_management.c
+++ src/sccp_management.c
@@ -239,15 +239,15 @@ void sccp_manager_eventListener(const sccp_event_t * event)
 					break;
 				case SCCP_FEATURE_CFWDALL:
 				case SCCP_FEATURE_CFWDBUSY:
-					manager_event(
-							EVENT_FLAG_CALL,
-							"CallForward",
-							"ChannelType: SCCP\r\nChannelObjectType: DeviceLine\r\nFeature: %s\r\nStatus: On\r\nExtension: %s\r\nSCCPLine: %s\r\nSCCPDevice: %s\r\n",
-							featureType2str(featureType),
-							(SCCP_FEATURE_CFWDALL == featureType) ? ((linedevice->cfwdAll.enabled) ? linedevice->cfwdAll.number : "(null)") : ((linedevice->cfwdBusy.enabled) ? linedevice->cfwdBusy.number : "(null)"),
-							(linedevice && linedevice->line) ? linedevice->line->name : "(null)",
-							DEV_ID_LOG(device)
-						);
+// 					manager_event(
+// 							EVENT_FLAG_CALL,
+// 							"CallForward",
+// 							"ChannelType: SCCP\r\nChannelObjectType: DeviceLine\r\nFeature: %s\r\nStatus: On\r\nExtension: %s\r\nSCCPLine: %s\r\nSCCPDevice: %s\r\n",
+// 							featureType2str(featureType),
+// 							(SCCP_FEATURE_CFWDALL == featureType) ? ((linedevice->cfwdAll.number) ? linedevice->cfwdAll.number : "(null)") : ((linedevice->cfwdBusy.enabled) ? linedevice->cfwdBusy.number : "(null)"),
+// 							(linedevice && linedevice->line) ? linedevice->line->name : "(null)",
+// 							DEV_ID_LOG(device)
+// 					);
 					break;
 				case SCCP_FEATURE_CFWDNONE:
 					manager_event(
