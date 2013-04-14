640ac8e55f432edde918592c58ad27d719b93e35
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-04-14 15:22:10 +0000

    Sync cli macro's for all version (removed alloca in astTrunk 3 weeks ago, without negative side effects)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4564 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast108.h
+++ src/pbx_impl/ast/ast108.h
@@ -1,5 +1,5 @@
 /*!
- * \file        ast106.h
+ * \file        ast108.h
  * \brief       SCCP PBX Asterisk Header
  * \author      Marcello Ceshia
  * \author      Diederik de Groot <ddegroot [at] users.sourceforge.net>
@@ -84,26 +84,23 @@ char *pbx_getformatname_multiple(char *buf, size_t size, format_t format);
 //   param4=registration description
 //   param5=usage string
 
-#define CLI_AMI_ENTRY(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE, _COMPLETER_REPEAT)			\
+#define CLI_AMI_ENTRY(_FUNCTION_NAME,_CALLED_FUNCTION,_DESCR,_USAGE, _COMPLETER_REPEAT)				\
 	static int manager_ ## _FUNCTION_NAME(struct mansession *s, const struct message *m)			\
 	{													\
 		const char *id = astman_get_header(m, "ActionID");						\
-/*		static char *cli_ami_command[] = { CLI_COMMAND, NULL};					*/	\
-/*		static char *ami_command = AMI_COMMAND;							*/	\
-/*		cli_ami_command[0] = ami_command;							*/	\
-		char *cli_ami_params[] = { CLI_COMMAND, CLI_AMI_PARAMS };				\
-		char *arguments[ARRAY_LEN(cli_ami_params)];						\
-		uint8_t x=0; 											\
-		uint8_t i=0; 											\
+		char *cli_ami_params[] = { CLI_COMMAND, CLI_AMI_PARAMS };					\
+		char *arguments[ARRAY_LEN(cli_ami_params)];							\
+		uint8_t x = 0, i = 0; 										\
 		for (x=0, i=0;x<ARRAY_LEN(cli_ami_params);x++) {						\
 			if(NULL != cli_ami_params[x] && strlen(cli_ami_params[x]) > 0){				\
-				arguments[i++]=sccp_strdupa(astman_get_header(m, cli_ami_params[x]));		\
+				arguments[i++]=(char *)astman_get_header(m, cli_ami_params[x]);		 	\
 			} 											\
 		}												\
 		char idtext[256] = "";										\
 		int total = 0;											\
-		if (!pbx_strlen_zero(id))									\
+		if (!pbx_strlen_zero(id)) {									\
 			snprintf(idtext, sizeof(idtext), "ActionID: %s\r\n", id);				\
+		}												\
 		astman_send_ack(s, m, AMI_COMMAND);								\
 		_CALLED_FUNCTION(-1, &total, s, m, ARRAY_LEN(arguments), arguments);				\
 		astman_append(s,										\
@@ -160,7 +157,7 @@ char *pbx_getformatname_multiple(char *buf, size_t size, format_t format);
 		static char command[80]="";									\
 		if (cmd == CLI_INIT) {										\
 		 	ast_join(command, sizeof(command), cli_command);					\
-			e->command = command;								\
+			e->command = command;									\
 			e->usage = _USAGE;									\
 			return NULL;										\
 		} else if (cmd == CLI_GENERATE) {								\
