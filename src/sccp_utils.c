e7f8e5f070db6d386e214b63062661bb3356b1de
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2013-10-13 12:08:57 +0000

    fixing sccp_strcaseequals
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@4898 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_utils.c
+++ src/sccp_utils.c
@@ -1109,7 +1109,7 @@ gcc_inline boolean_t sccp_strcaseequals(const char *data1, const char *data2)
 {
 	if (sccp_strlen_zero(data1) && sccp_strlen_zero(data2)) {
 		return TRUE;
-	} else if (!sccp_strlen_zero(data1) && !sccp_strlen_zero(data2)) {
+	} else if (!sccp_strlen_zero(data1) && !sccp_strlen_zero(data2) && ( sccp_strlen(data1) == sccp_strlen(data2) ) ) {
 		return !strcasecmp(data1, data2);
 	}
 	return FALSE;
