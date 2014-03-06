a49ed648a672b678e556e740aa61371f745245fd
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2014-03-06 18:09:14 +0000

    894x phone apparently support 10 speeddials with only 4 buttons (6 displayed in a dropdown menu) (Bug: #219/Reported by hhornung).
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@5312 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_device.c
+++ src/sccp_device.c
@@ -786,7 +786,7 @@ void sccp_dev_build_buttontemplate(sccp_device_t * d, btnlist * btn)
 			sccp_softkey_setSoftkeyState(d, KEYMODE_CONNTRANS, SKINNY_LBL_VIDEO_MODE, TRUE);
 #endif
 			d->hasDisplayPrompt = sccp_device_falseResult;
-			for (i = 0; i < 4; i++) {
+			for (i = 0; i < 10; i++) {					// 4 visible, 6 in dropdown
 				(btn++)->type = SCCP_BUTTONTYPE_MULTI;
 			}
 			(btn++)->type = SKINNY_BUTTONTYPE_CONFERENCE;
