c5c2f259536031ce15d848b161ae8f293549c18b
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2014-06-27 11:06:13 +0000

    Removed dtmfmode from conf/doc examples
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@5529 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- conf/mysql-v5.sql
+++ conf/mysql-v5.sql
@@ -17,7 +17,6 @@ CREATE TABLE IF NOT EXISTS `sccpdevice` (
   `transfer` varchar(5) default 'on',
   `cfwdall` varchar(5) default 'on',
   `cfwdbusy` varchar(5) default 'on',
-  `dtmfmode` varchar(10) default NULL,
   `imageversion` varchar(45) default NULL,
   `deny` varchar(45) default NULL,
   `permit` varchar(45) default NULL,
