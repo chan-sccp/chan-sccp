52dd6e81cf121b549feffed56e5562d8fd10798c
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2014-07-02 18:08:30 +0000

    Remove AST_RTP_PROPERTY_DTMF_COMPENSATE when running in RFC2833 mode, Allowing non fixed length DTMF tones (Checked/Tested by Pavel).
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@5560 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast113.c
+++ src/pbx_impl/ast/ast113.c
@@ -2141,7 +2141,9 @@ static boolean_t sccp_wrapper_asterisk113_create_audio_rtp(sccp_channel_t * c)
 		ast_rtp_instance_set_prop(c->rtp.audio.rtp, AST_RTP_PROPERTY_RTCP, 1);
 
 		ast_rtp_instance_set_prop(c->rtp.audio.rtp, AST_RTP_PROPERTY_DTMF, 1);
-		ast_rtp_instance_set_prop(c->rtp.audio.rtp, AST_RTP_PROPERTY_DTMF_COMPENSATE, 1);
+		if (c->dtmfmode == SCCP_DTMF_SKINNY) {
+			ast_rtp_instance_set_prop(c->rtp.audio.rtp, AST_RTP_PROPERTY_DTMF_COMPENSATE, 1);
+		}
 
 		ast_channel_set_fd(c->owner, 0, ast_rtp_instance_fd(c->rtp.audio.rtp, 0));
 		ast_channel_set_fd(c->owner, 1, ast_rtp_instance_fd(c->rtp.audio.rtp, 1));
