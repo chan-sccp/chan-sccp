8591dc622cf83cddce78b8bda405fb724102ad3a
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2014-08-04 14:45:09 +0000

    Fix: gpickup via pickup extension (ie: *8). (Reported by Ian)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@5624 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast106.c
+++ src/pbx_impl/ast/ast106.c
@@ -1248,7 +1248,7 @@ static boolean_t sccp_wrapper_asterisk16_getPickupExtension(const sccp_channel_t
 {
 	boolean_t res = FALSE;
 	if (!sccp_strlen_zero(ast_pickup_ext())) {
-		*extension = (char *)ast_pickup_ext();
+		*extension = strdup((char *)ast_pickup_ext());
 		res = TRUE;
 	}
 	return res;
