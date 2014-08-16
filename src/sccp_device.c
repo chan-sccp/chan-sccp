3f0b438705f31f3202616ce648e91a90dd0a28c7
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2014-08-16 15:39:27 +0000

    Fix: compile warning because of unused variable
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@5666 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_device.c
+++ src/sccp_device.c
@@ -1994,7 +1994,8 @@ void sccp_dev_clean(sccp_device_t * device, boolean_t remove_from_global, uint8_
 			sccp_device_sendReset(d, SKINNY_DEVICE_RESTART);
 			usleep(20);
 			if (d->session) {
-				AUTO_RELEASE sccp_device_t *previous_device = sccp_session_removeDevice(d->session);
+				sccp_device_t *previous_device = sccp_session_removeDevice(d->session);
+				previous_device = previous_device ? sccp_device_release(previous_device) : NULL;
 			}
 			d->session = NULL;
 		}
