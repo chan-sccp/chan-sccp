dd6f921a6e569fe7abf039f6fae03cfea2e17e14
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-01-16 19:34:37 +0000

    remove PBX(set_callstate) (c, AST_STATE_BUSY) from sccp_indicate and move it to the right places
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@5850 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast106.c
+++ src/pbx_impl/ast/ast106.c
@@ -583,6 +583,7 @@ static int sccp_wrapper_asterisk16_indicate(PBX_CHANNEL_TYPE * ast, int ind, con
 			break;
 		case AST_CONTROL_BUSY:
 			sccp_indicate(d, c, SCCP_CHANNELSTATE_BUSY);
+			PBX(set_callstate) (c, AST_STATE_BUSY);
 			break;
 		case AST_CONTROL_CONGESTION:
 			sccp_indicate(d, c, SCCP_CHANNELSTATE_CONGESTION);
