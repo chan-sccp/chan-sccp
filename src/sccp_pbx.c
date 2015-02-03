43cbb2676878f1a5b41b0a717d6e28ee650a8279
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-02-03 12:04:17 +0000

    Fix: directed pickup. Issue reported by Viktu Pons Colomer.
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@5900 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_pbx.c
+++ src/sccp_pbx.c
@@ -965,11 +965,9 @@ void *sccp_pbx_softswitch(sccp_channel_t * channel)
 
 				if (!sccp_strlen_zero(shortenedNumber)) {
 					sccp_log((DEBUGCAT_CORE)) (VERBOSE_PREFIX_3 "SCCP: (sccp_pbx_softswitch) Asterisk request to pickup exten '%s'\n", shortenedNumber);
-					/*
-					if (!sccp_feat_directed_pickup(c, shortenedNumber)) {
+					if (sccp_feat_directed_pickup(c, shortenedNumber)) {
 						sccp_indicate(d, c, SCCP_CHANNELSTATE_INVALIDNUMBER);
 					}
-					*/
 					PBX(set_callstate) (c, AST_STATE_UP);
 				} else {
 					// without a number we can also close the call. Isn't it true ?
