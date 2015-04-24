b22a8aeb9e11b906be133dacb60174cc68014377
marcelloceschia <marcelloceschia@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-04-24 09:20:04 +0000

    no need to expose sccp_mailbox_subscriber_list, it a private data structure; check mwi line state in sccp_mwi_setMWILineStatus and not the mwi light (instance 0)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6035 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_mwi.h
+++ src/sccp_mwi.h
@@ -17,68 +17,6 @@
 #include "asterisk/event.h"
 #endif
 
-/*!
- * \brief SCCP Mailbox Line Type Definition
- *
- * holding line information for mailbox subscription
- *
- */
-typedef struct sccp_mailboxLine sccp_mailboxLine_t;
-
-/*!
- * \brief SCCP Mailbox Line Type Structure
- *
- * holding line information for mailbox subscription
- *
- */
-struct sccp_mailboxLine {
-	sccp_line_t *line;
-	SCCP_LIST_ENTRY (sccp_mailboxLine_t) list;
-};
-
-/*!
- * \brief SCCP Mailbox Subscriber List Type Definition
- */
-typedef struct sccp_mailbox_subscriber_list sccp_mailbox_subscriber_list_t;
-
-/*!
- * \brief SCCP Mailbox Subscriber List Structure
- *
- * we hold a mailbox event subscription in sccp_mailbox_subscription_t.
- * Each line that holds a subscription for this mailbox is listed in
- */
-struct sccp_mailbox_subscriber_list {
-	char mailbox[60];
-	char context[60];
-
-	SCCP_LIST_HEAD (, sccp_mailboxLine_t) sccp_mailboxLine;
-	SCCP_LIST_ENTRY (sccp_mailbox_subscriber_list_t) list;
-
-	/*!
-	 * \brief Current Voicemail Statistic Structure
-	 */
-	struct {
-		int newmsgs;											/*!< New Messages */
-		int oldmsgs;											/*!< Old Messages */
-	} currentVoicemailStatistic;								/*!< Current Voicemail Statistic Structure */
-
-	/*!
-	 * \brief Previous Voicemail Statistic Structure
-	 */
-	struct {
-		int newmsgs;											/*!< New Messages */
-		int oldmsgs;											/*!< Old Messages */
-	} previousVoicemailStatistic;								/*!< Previous Voicemail Statistic Structure */
-
-#if defined ( CS_AST_HAS_EVENT ) || (defined( CS_AST_HAS_STASIS ))
-	/*!
-	 * \brief Asterisk Event Subscribers Structure
-	 */
-	struct pbx_event_sub *event_sub;
-#else
-	int schedUpdate;
-#endif
-};																/*!< SCCP Mailbox Subscriber List Structure */
 
 void sccp_mwi_module_start(void);
 void sccp_mwi_module_stop(void);
