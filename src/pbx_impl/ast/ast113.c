5a96497da07b519d02b36a689738ffb4f8e3c163
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-06-09 19:41:08 +0000

    Fix compile warning about missing declaration
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6115 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast113.c
+++ src/pbx_impl/ast/ast113.c
@@ -3349,7 +3349,7 @@ int pbx_manager_register(const char *action, int authority, int (*func) (struct 
 	return ast_manager_register2(action, authority, func, ast_module_info->self, synopsis, description);
 }
 
-boolean_t sccp_wrapper_asterisk113_setLanguage(PBX_CHANNEL_TYPE * pbxChannel, const char *language)
+static boolean_t sccp_wrapper_asterisk113_setLanguage(PBX_CHANNEL_TYPE * pbxChannel, const char *language)
 {
 
 	ast_channel_language_set(pbxChannel, language);
