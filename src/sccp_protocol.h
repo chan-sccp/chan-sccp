e77ddff38ed36f180d753a0ab61b20d3d8fb2b8b
Diederik de Groot <ddegroot@talon.nl>
2015-09-05 08:52:23 +0200

    Refine comments in sccp_protocol (unrelated)

--- src/sccp_protocol.h
+++ src/sccp_protocol.h
@@ -2096,7 +2096,7 @@ typedef union {
 				uint32_t lel_millisecondPacketSize;						/*!< Packet Size per MilliSecond */
 				uint32_t lel_payloadType;							/*!< Media_PayloadType */
 				uint32_t lel_precedenceValue;							/*!< Precedence Value */
-				uint32_t lel_ssValue;								/*!< Simple String Value */
+				uint32_t lel_ssValue;								/*!< Silence Suppression Value */
 				uint32_t lel_maxFramesPerPacket;						/*!< Maximum Frames per Packet */
 				uint32_t lel_g723BitRate;							/*!< only used with G.723 payload */
 				uint32_t lel_callReference;							/*!< Conference ID 1 */
@@ -2133,7 +2133,7 @@ typedef union {
 				uint32_t lel_millisecondPacketSize;						/*!< Packet Size per Millisecond */
 				uint32_t lel_payloadType;							/*!< Media_PayloadType */
 				uint32_t lel_precedenceValue;							/*!< Precedence Value */
-				uint32_t lel_ssValue;								/*!< Simple String Value */
+				uint32_t lel_ssValue;								/*!< Silence Suppression Value */
 				uint32_t lel_maxFramesPerPacket;						/*!< Maximum Frames per Packet */
 				uint32_t lel_g723BitRate;							/*!< G.723 BitRate (only used with G.723 payload) */
 				uint32_t lel_callReference;							/*!< Conference ID 1 */
@@ -2157,7 +2157,7 @@ typedef union {
 				uint32_t lel_millisecondPacketSize;						/*!< Packet Size per Millisecond */
 				uint32_t lel_payloadType;							/*!< Media_PayloadType */
 				uint32_t lel_precedenceValue;							/*!< Precedence Value */
-				uint32_t lel_ssValue;								/*!< Simple String Value */
+				uint32_t lel_ssValue;								/*!< Silence Suppression Value */
 				uint32_t lel_maxFramesPerPacket;						/*!< Maximum Frames per Packet */
 				uint32_t lel_g723BitRate;							/*!< G.723 BitRate (only used with G.723 payload) */
 				uint32_t lel_callReference;							/*!< Conference ID 1 */
@@ -2537,7 +2537,7 @@ typedef union {
 				uint32_t lel_passThruPartyId;							/*!< Pass Through Party ID */
 				uint32_t lel_millisecondPacketSize;						/*!< Millisecond Packet Size */
 				uint32_t lel_payloadType;							/*!< Media_Payload Type */
-				uint32_t lel_vadValue;								/*!< VAD Value */
+				uint32_t lel_vadValue;								/*!< Voice Activity Detection Value */
 				uint32_t lel_g723BitRate;							/*!< G.723 Payload (Only applies to G.723) */
 				/* protocol version 5 fields */
 				uint32_t lel_callReference;							/*!< Conference ID */
@@ -2569,7 +2569,7 @@ typedef union {
 				uint32_t lel_passThruPartyId;							/*!< Pass Through Party ID */
 				uint32_t lel_millisecondPacketSize;						/*!< Millisecond Packet Size */
 				uint32_t lel_payloadType;							/*!< Media_Payload Type */
-				uint32_t lel_vadValue;								/*!< VAD Value */
+				uint32_t lel_vadValue;								/*!< Voice Activity Detection Value */
 				uint32_t lel_g723BitRate;							/*!< G.723 Payload (Only applies to G.723) */
 				/* protocol version 5 fields */
 				uint32_t lel_callReference;							/*!< Conference ID */
@@ -2592,7 +2592,7 @@ typedef union {
 				uint32_t lel_passThruPartyId;							/*!< Pass Through Party ID */
 				uint32_t lel_millisecondPacketSize;						/*!< Millisecond Packet Size */
 				uint32_t lel_payloadType;							/*!< Media_Payload Type */
-				uint32_t lel_vadValue;								/*!< VAD Value */
+				uint32_t lel_vadValue;								/*!< Voice Activity Detection Value */
 				uint32_t lel_g723BitRate;							/*!< G.723 Payload (Only applies to G.723) */
 				/* protocol version 5 fields */
 				uint32_t lel_callReference;							/*!< Conference ID */
