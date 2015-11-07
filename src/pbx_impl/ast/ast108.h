776e2b17c662390bc975a2601c7f8d6c1ecbbccd
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-11-07 18:25:02 +0000

    Fix compile warnings when used outside of svn repository. Fix compile warning for empty definition of ast_bridge_lock in asterisk < 10
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6358 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast108.h
+++ src/pbx_impl/ast/ast108.h
@@ -36,11 +36,12 @@ int sccp_wrapper_asterisk_set_rtp_peer(PBX_CHANNEL_TYPE * ast, PBX_RTP_TYPE * rt
 //int sccp_restart_monitor(void);
 char *pbx_getformatname(format_t format);
 char *pbx_getformatname_multiple(char *buf, size_t size, format_t format);
+static inline void __do_nothing(void) {}									// will be optimized out
 
 #undef pbx_bridge_lock
 #undef pbx_bridge_unlock
-#define pbx_bridge_lock(_x) NULL
-#define pbx_bridge_unlock(_x) NULL
+#define pbx_bridge_lock(x) __do_nothing()
+#define pbx_bridge_unlock(x) __do_nothing()
 
 #define pbx_channel_name(x) x->name
 
