5b61722c16b4de5e5cd1cc3702815405f6695928
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-11-07 15:42:04 +0000

    Enh: Added test cases to sccp_callinfo
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6347 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_callinfo.h
+++ src/sccp_callinfo.h
@@ -54,4 +54,10 @@ int sccp_callinfo_setLastRedirectingParty(sccp_callinfo_t * const ci, const char
 /* debug */
 boolean_t sccp_callinfo_getCallInfoStr(const sccp_callinfo_t * const ci, pbx_str_t ** const buf);
 void sccp_callinfo_print2log(const sccp_callinfo_t * const ci, const char *const header);
+
+/* tests */
+#if CS_TEST_FRAMEWORK
+void sccp_callinfo_register_tests(void);
+void sccp_callinfo_unregister_tests(void);
+#endif
 // kate: indent-width 8; replace-tabs off; indent-mode cstyle; auto-insert-doxygen on; line-numbers on; tab-indents on; keep-extra-spaces off; auto-brackets off;
