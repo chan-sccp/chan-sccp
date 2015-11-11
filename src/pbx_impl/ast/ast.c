09bc0b11036ecae9e175ef533ebcfc6b3adcc579
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-11-11 17:18:26 +0000

    Fix: compile issue with asterisk-1.8 and below.
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6381 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/pbx_impl/ast/ast.c
+++ src/pbx_impl/ast/ast.c
@@ -732,6 +732,7 @@ void sccp_asterisk_connectedline(sccp_channel_t * channel, const void *data, siz
 			SCCP_CALLINFO_KEY_SENTINEL);
 	} else {
 		if (channel->calltype == SKINNY_CALLTYPE_INBOUND) {
+#if ASTERISK_VERSION_GROUP > 108
 			struct ast_party_id redirecting_orig = pbx_channel_redirecting_effective_orig(ast);
 			if (!redirecting_orig.name.valid && !redirecting_orig.number.valid) {
 				changes = sccp_callinfo_setter(callInfo,
@@ -746,6 +747,12 @@ void sccp_asterisk_connectedline(sccp_channel_t * channel, const void *data, siz
 					SCCP_CALLINFO_ORIG_CALLEDPARTY_NUMBER, redirecting_orig.number.valid ? ast_channel_redirecting(ast)->orig.number.str : "",
 					SCCP_CALLINFO_KEY_SENTINEL);
 			}
+#else
+			changes = sccp_callinfo_setter(callInfo,
+				SCCP_CALLINFO_CALLINGPARTY_NUMBER, pbx_channel_connected_id(ast).number.str,
+				SCCP_CALLINFO_CALLINGPARTY_NAME, pbx_channel_connected_id(ast).name.str,
+				SCCP_CALLINFO_KEY_SENTINEL);
+#endif
 		} else {
 			changes = sccp_callinfo_setter(callInfo,
 				SCCP_CALLINFO_CALLEDPARTY_NUMBER, pbx_channel_connected_id(ast).number.str,
