a25ce8a47c8427374f2144511699daa186dc3403
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2015-11-20 02:47:38 +0000

    Fix: clang-3.7+ compilation blocks call requires cast to be accepted by ami hooks function. Fix: clang-3.8+ Passing a local variable is converted to const if not tagged with __block storage qualifier Enh: Changed sccp_manager_action2char to return an allocated char *, which needs to be freed (only if returning success)
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6413 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_management.h
+++ src/sccp_management.h
@@ -22,6 +22,6 @@ int sccp_unregister_management(void);
 void sccp_manager_module_start(void);
 void sccp_manager_module_stop(void);
 
-int sccp_manager_action2pbx_str(struct ast_str *outStr, const char *manager_command);
+int sccp_manager_action2str(const char *manager_command, char **outStr);
 #endif
 // kate: indent-width 8; replace-tabs off; indent-mode cstyle; auto-insert-doxygen on; line-numbers on; tab-indents on; keep-extra-spaces off; auto-brackets off;
