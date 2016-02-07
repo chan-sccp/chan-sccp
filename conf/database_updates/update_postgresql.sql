1f5ae41fb2d1a04c7ae4ca822d035a398b6d8245
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2016-02-07 18:49:41 +0000

    Sync/Update/Fix the database update scripts
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6601 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- conf/database_updates/update_postgresql.sql
+++ conf/database_updates/update_postgresql.sql
@@ -1,14 +1,17 @@
 
-DROP sccpdeviceconfig;
+DROP VIEW sccpdeviceconfig;
 
 ALTER TABLE sccpdevice alter earlyrtp type varchar(10);
-ALTER TABLE sccpline alter dnd type varchar(7) default "reject";
-ALTER TABLE `sccpdevice` DROP COLUMN `dtmfmode`;
+ALTER TABLE sccpline alter dnd type varchar(7);
+ALTER TABLE sccpline alter dnd set default 'reject';
+ALTER TABLE sccpdevice alter nat type varchar(7);
+ALTER TABLE sccpdevice alter nat set default 'auto';
+ALTER TABLE sccpdevice DROP COLUMN dtmfmode;
 
 CREATE OR REPLACE VIEW sccpdeviceconfig AS
         SELECT 
-              (SELECT textcat_column(bc.type || ',' || bc.name || COALESCE(',' || bc.options, '') || ';') FROM (SELECT * FROM buttonconfig WHERE device=sccpdevice.name ORDER BY instance) bc ) as button,
-              sccpdevice.*
+                (SELECT textcat_column(bc.type || ',' || bc.name || COALESCE(',' || bc.options, '') || ';') FROM (SELECT * FROM buttonconfig WHERE device=sccpdevice.name ORDER BY instance) bc ) as button,
+                sccpdevice.*
         FROM sccpdevice
 ;
                                                 
