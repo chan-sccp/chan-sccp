da0a06f10258c70187d67550bf86c0bac4be088c
ddegroot <ddegroot@43c1dc8c-776e-4df2-8ef1-6e829299be21>
2016-02-25 00:53:51 +0000

    Use __strcpy in this case
    git-svn-id: https://svn.code.sf.net/p/chan-sccp-b/code/trunk@6692 43c1dc8c-776e-4df2-8ef1-6e829299be21

--- src/sccp_utils.c
+++ src/sccp_utils.c
@@ -2424,7 +2424,7 @@ static char **__sccp_bt_get_symbols(void **addresses, size_t num_frames)
 			}
 			strings = tmp;
 			strings[stackfr] = (char *) strings + strings_size;
-			strcpy(strings[stackfr], msg);/* Safe since we just allocated the room. */
+			__strcpy(strings[stackfr], msg);/* Safe since we just allocated the room. */
 			//sccp_copy_string(strings[stackfr], msg, strings_size + eachlen[stackfr]);
 			strings_size += eachlen[stackfr];
 		}
