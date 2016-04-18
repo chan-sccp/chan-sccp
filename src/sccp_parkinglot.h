1e0d35e31d0c69d1876e08f66286cfa3df7ea2cb
Diederik de Groot <dkgroot@talon.nl>
2016-04-18 12:35:32 +0200

    Include sccp_parkinglot.c and sccp_parkinglot.h

---
+++ src/sccp_parkinglot.h
@@ -0,0 +1,30 @@
+/*!
+ * \file	sccp_parkinglot.h
+ * \brief	SCCP ParkingLot Header
+ * \author	Diederik de Groot <ddegroot [at] users.sf.net>
+ * \date	2015-Sept-16
+ * \note	This program is free software and may be modified and distributed under the terms of the GNU Public License.
+ *		See the LICENSE file at the top of the source tree.
+ *
+ * $date$
+ * $revision$
+ */
+#pragma once
+
+// forward declarations
+struct parkinglot;
+typedef struct parkinglot sccp_parkinglot_t;
+
+__BEGIN_C_EXTERN__
+typedef struct {
+	int (*attachObserver)(const char *parkinglot, sccp_device_t * device, uint8_t instance);
+	int (*detachObserver)(const char *parkinglot, sccp_device_t * device, uint8_t instance);
+	int (*addSlot)(const char *parkinglot, int slot, struct message *m);
+	int (*removeSlot)(const char *parkinglot, int slot);
+	void (*showCXML) (const char *parkinglot, constDevicePtr d, uint8_t instance);
+} ParkingLotInterface;
+
+extern const ParkingLotInterface iParkingLot;
+
+__END_C_EXTERN__
+// kate: indent-width 8; replace-tabs off; indent-mode cstyle; auto-insert-doxygen on; line-numbers on; tab-indents on; keep-extra-spaces off; auto-brackets off;
