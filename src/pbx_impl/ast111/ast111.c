d32e24d803cca8399972d930c972f51b0f7fe614
Diederik de Groot <dkgroot@talon.nl>
2017-09-12 13:33:20 +0200

    Fix: segfault on originate when using asterisk-11
    References #411

--- src/pbx_impl/ast111/ast111.c
+++ src/pbx_impl/ast111/ast111.c
@@ -1326,7 +1326,7 @@ static PBX_CHANNEL_TYPE *sccp_wrapper_asterisk111_request(const char *type, stru
 		options++;
 	}
 
-	sccp_log(DEBUGCAT_CHANNEL) (VERBOSE_PREFIX_3 "SCCP: Asterisk asked us to create a channel with type=%s, format=" UI64FMT ", lineName=%s, options=%s, requestor: %s\n", type, (ULONG) ast_format_cap_to_old_bitfield(format), lineName, (options) ? options : "", pbx_channel_name(requestor));
+	sccp_log(DEBUGCAT_CHANNEL) (VERBOSE_PREFIX_3 "SCCP: Asterisk asked us to create a channel with type=%s, format=" UI64FMT ", lineName=%s, options=%s, requestor: %s\n", type, (ULONG) ast_format_cap_to_old_bitfield(format), lineName, (options) ? options : "", requestor ? pbx_channel_name(requestor) : "");
 	if (requestor) {							/* get ringer mode from ALERT_INFO */
 		sccp_parse_alertinfo((PBX_CHANNEL_TYPE *)requestor, &ringermode);
 	}
