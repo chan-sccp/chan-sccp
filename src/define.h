d67eb916f7664df16bedb026c0a959774fa7191a
Diederik de Groot <dkgroot@talon.nl>
2019-01-06 22:15:59 +0100

    Fix compile warning for container_of

--- src/define.h
+++ src/define.h
@@ -259,5 +259,5 @@ static inline void sccp_raii_cleanup_block(sccp_raii_cleanup_block_t *b) { (*b)(
 
 #define container_of(ptr, type, member) ({                      \
         const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
-        (type *)( (char *)__mptr - offsetof(type,member) );})
+        (type *)( (void *)__mptr - offsetof(type,member) );})
 // kate: indent-width 8; replace-tabs off; indent-mode cstyle; auto-insert-doxygen on; line-numbers on; tab-indents on; keep-extra-spaces off; auto-brackets off;
