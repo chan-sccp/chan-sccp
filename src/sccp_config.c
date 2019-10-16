chan-sccp/chan-sccp

commit 09406a5080e724d91ab6ebf64740e32f0edf136d

Add cli command to generate a wiki page containing all sccp.conf parameters
References #482

Signed-off-by: Diederik de Groot <dkgroot@talon.nl>

src/sccp_config.c
@@ -3358,6 +3358,117 @@ int sccp_manager_config_metadata(struct mansession *s, const struct message *m)
 	return 0;
 }
 
+static int _config_generate_wiki(char * filename, int configType)
+{
+	const SCCPConfigSegment * sccpConfigSegment = NULL;
+	const SCCPConfigOption * config = NULL;
+	long unsigned int sccp_option;
+	long unsigned int segment;
+	char * description = "";
+	char * description_part = "";
+	char fn[PATH_MAX];
+
+	snprintf(fn, sizeof(fn), "%s/%s", ast_config_AST_CONFIG_DIR, filename);
+	pbx_log(LOG_NOTICE, "Creating new wiki file '%s'\n", fn);
+
+	FILE * f = NULL;
+
+	if(!(f = fopen(fn, "w"))) {
+		pbx_log(LOG_ERROR, "Error creating new config file \n");
+		return 1;
+	}
+
+	char date[256] = "";
+	time_t t;
+
+	time(&t);
+	sccp_copy_string(date, ctime(&t), sizeof(date));
+
+	fprintf(f, "*sccp.conf options*\n\n");
+	for(segment = SCCP_CONFIG_GLOBAL_SEGMENT; segment <= SCCP_CONFIG_SOFTKEY_SEGMENT; segment++) {
+		sccpConfigSegment = sccp_find_segment((sccp_config_segment_t)segment);
+		fprintf(f, "\n**[%s] section**\n\n", sccpConfigSegment->name);
+		fprintf(f, "<table>\n");
+		fprintf(f, "<tr><td><b>parameter</b></td><td><b>default</b></td><td><b>format</b></td><td><b>required</b></td><td><b>status</b></td></tr>\n");
+		fprintf(f, "<tr><td></td><td colspan='4'><b>description</b></td></tr>\n");
+		config = sccpConfigSegment->config;
+		for(sccp_option = 0; sccp_option < sccpConfigSegment->config_size; sccp_option++) {
+			// if ((config[sccp_option].flags & (SCCP_CONFIG_FLAG_IGNORE | SCCP_CONFIG_FLAG_DEPRECATED | SCCP_CONFIG_FLAG_OBSOLETE)) == 0) {
+			sccp_log((DEBUGCAT_CONFIG))(VERBOSE_PREFIX_2 "adding name: %s, default_value: %s\n", config[sccp_option].name, config[sccp_option].defaultValue);
+			if(!sccp_strlen_zero(config[sccp_option].name)) {
+				char delims[] = "|";
+				char * option_name_tokens = pbx_strdupa(config[sccp_option].name);
+				char * option_value_tokens = NULL;
+				if(!sccp_strlen_zero(config[sccp_option].defaultValue)) {
+					option_value_tokens = pbx_strdupa(config[sccp_option].defaultValue);
+				} else {
+					option_value_tokens = pbx_strdupa("\"\"");
+				}
+				char * option_name_tokens_saveptr = NULL;
+				char * option_value_tokens_saveptr = NULL;
+				char * option_name = strtok_r(option_name_tokens, delims, &option_name_tokens_saveptr);
+				char * option_value = strtok_r(option_value_tokens, delims, &option_value_tokens_saveptr);
+				while(option_name != NULL) {
+					fprintf(f, "<tr class=option_row id='%s'>\n", option_name);
+					fprintf(f, "<td class='name'>%s</td><td class='default_value'>%s</td>\n", option_name, option_value);
+					option_name = strtok_r(NULL, delims, &option_name_tokens_saveptr);
+					option_value = strtok_r(NULL, delims, &option_value_tokens_saveptr);
+					switch(config[sccp_option].type) {
+						case SCCP_CONFIG_DATATYPE_STRING:
+							fprintf(f, "<td class='format'>max length:%d</td>\n", (int)config[sccp_option].size - 1);
+							break;
+						case SCCP_CONFIG_DATATYPE_ENUM: {
+							char * all_entries = pbx_strdupa(config[sccp_option].all_entries());
+							char * possible_entry = "";
+							int subcomma = 0;
+
+							fprintf(f, "<td class='format'><small>potential values:[");
+							while(all_entries && (possible_entry = strsep(&all_entries, ","))) {
+								fprintf(f, "%s%s", subcomma ? ", " : "", possible_entry);
+								subcomma = 1;
+							}
+							fprintf(f, "]</small></td>\n");
+						} break;
+						default:
+							fprintf(f, "<td class='format'>-</td>\n");
+							break;
+					}
+					fprintf(f, "<td class='required'>%s</td>\n", (config[sccp_option].flags & SCCP_CONFIG_FLAG_REQUIRED) == SCCP_CONFIG_FLAG_REQUIRED ? "yes" : "-");
+					fprintf(f, "<td class='status'>%s</td> ",
+						(config[sccp_option].flags & SCCP_CONFIG_FLAG_DEPRECATED) == SCCP_CONFIG_FLAG_DEPRECATED
+						    ? "deprecated"
+						    : (config[sccp_option].flags & SCCP_CONFIG_FLAG_OBSOLETE) == SCCP_CONFIG_FLAG_OBSOLETE ? "obsolete" : "-");
+					fprintf(f, "</tr>\n");
+					if(!sccp_strlen_zero(config[sccp_option].description)) {
+						fprintf(f, "<tr class='descr_row'><td></td>\n");
+						fprintf(f, "<td class='description' id='%s' colspan='4'><small>", config[sccp_option].name);
+						description = pbx_strdupa(config[sccp_option].description);
+						while((description_part = strsep(&description, "\n"))) {
+							if(!sccp_strlen_zero(description_part)) {
+								fprintf(f, "%s.<br>", description_part);
+							}
+						}
+						if(description_part) {
+							sccp_free(description_part);
+						}
+						fprintf(f, "</small></td>\n");
+					}
+				}
+				fprintf(f, "</tr>\n");
+			} else {
+				pbx_log(LOG_ERROR, "Error creating new variable structure for %s='%s'\n", config[sccp_option].name, config[sccp_option].defaultValue);
+				fclose(f);
+				return 2;
+			}
+		}
+		fprintf(f, "</table><br>\n");
+	}
+	fclose(f);
+	pbx_log(LOG_NOTICE, "Created new config file '%s'\n", fn);
+
+	return 0;
+};
+
 /*!
  * \brief Generate default sccp.conf file
  * \param filename Filename
@@ -3369,6 +3480,9 @@ int sccp_manager_config_metadata(struct mansession *s, const struct message *m)
  */
 int sccp_config_generate(char *filename, int configType)
 {
+	if(configType == 3) {
+		return _config_generate_wiki(filename, configType);
+	}
 	const SCCPConfigSegment * sccpConfigSegment = NULL;
 	const SCCPConfigOption * config = NULL;
 	long unsigned int sccp_option;
@@ -3525,7 +3639,7 @@ int sccp_config_generate(char *filename, int configType)
 		sccp_log((DEBUGCAT_CONFIG)) ("\n");
 	}
 	fclose(f);
-	pbx_log(LOG_NOTICE, "Created new config file '%s'\n", fn);
+	pbx_log(LOG_NOTICE, "Created new wiki file '%s'\n", fn);
 
 	return 0;
 };

src/sccp_cli.c
@@ -2977,17 +2977,21 @@ static int sccp_cli_config_generate(int fd, int argc, char *argv[])
 {
 	int returnval = RESULT_FAILURE;
 	char *config_file = "sccp.conf.new";
+	int option = 0;
 
-	if (argc < 2 || argc > 4) {
+	if(argc < 2 || argc > 5) {
 		return RESULT_SHOWUSAGE;
 	}
 
 	pbx_cli(fd, "SCCP: Generating new config file.\n");
 
-	if (argc == 4) {
+	if(argc >= 4) {
 		config_file = pbx_strdupa(argv[3]);
 	}
-	if (!sccp_config_generate(config_file, 0)) {
+	if(argc == 5 && sccp_strcaseequals(argv[4], "wiki")) {
+		option = 3;
+	}
+	if(!sccp_config_generate(config_file, option)) {
 		returnval = RESULT_SUCCESS;
 	} else {
 		pbx_cli(fd, "SCCP generation failed.\n");
@@ -2996,7 +3000,8 @@ static int sccp_cli_config_generate(int fd, int argc, char *argv[])
 	return returnval;
 }
 
-static char config_generate_usage[] = "Usage: SCCP config generate [filename]\n" "       Generates a new sccp.conf if none exists. Either creating sccp.conf or [filename] if specified\n";
+static char config_generate_usage[] = "Usage: SCCP config generate [filename] [option]\n"
+				      "       Generates a new sccp.conf if none exists. Either creating sccp.conf or [filename] if specified\n";
 
 #ifndef DOXYGEN_SHOULD_SKIP_THIS
 #define CLI_COMMAND "sccp", "config", "generate"

src/sccp_config_entries.hh
@@ -30,7 +30,7 @@
 /* *INDENT-OFF* */
 static const SCCPConfigOption sccpGlobalConfigOptions[]={
 	{"debug", 			G_OBJ_REF(debug), 			TYPE_PARSER(sccp_config_parse_debug),						SCCP_CONFIG_FLAG_MULTI_ENTRY,					SCCP_CONFIG_NOUPDATENEEDED,		"core",				"console debug level or categories\n"
-																														  							"examples: debug = 11 | debug = mwi,event,core | debug = all | debug = none or 0\n"
+																														  							"examples: debug = 11; debug = mwi,event,core; debug = all; debug = none or 0\n"
 																																					"possible categories:\n"
 																																					"core, hint, rtp, device, line, action, channel, cli, config, feature, feature_button, softkey, indicate, pbx\n"
 																																					"socket, mwi, event, adv_feature, conference, buttontemplate, speeddial, codec, realtime, lock, parkinglot, newcode, high, all, none\n"},
@@ -332,19 +332,19 @@ static const SCCPConfigOption sccpLineConfigOptions[] = {
 static const SCCPConfigOption sccpSoftKeyConfigOptions[] = {
 	{"type",			0,	0, 				TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"softkeyset",										"This should be set to softkeyset"},
 	{"name", 			0, 	0, 				TYPE_STRING,									SCCP_CONFIG_FLAG_IGNORE,					SCCP_CONFIG_NOUPDATENEEDED,		"default",										"softkeyset name\n"},
-	{"onhook",			S_OBJ_REF(modes[KEYMODE_ONHOOK]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial,newcall,cfwdall,dnd,pickup,gpickup,private",					"displayed when we are on hook"},
-	{"connected",			S_OBJ_REF(modes[KEYMODE_CONNECTED]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"hold,endcall,park,vidmode,select,cfwdall,cfwdbusy,idivert,monitor",			"displayed when we have a connected call"},
-	{"onhold",			S_OBJ_REF(modes[KEYMODE_ONHOLD]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"resume,newcall,endcall,transfer,conflist,select,dirtrfr,idivert,meetme",		"displayed when we have a call on hold"},
-	{"ringin",			S_OBJ_REF(modes[KEYMODE_RINGIN]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"answer,endcall,transvm,idivert",							"displayed when we have an incoming call"},
-	{"offhook",			S_OBJ_REF(modes[KEYMODE_OFFHOOK]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial,endcall,private,cfwdall,cfwdbusy,pickup,gpickup,meetme,barge",			"displayed when the phone is taken off hook"},
-	{"conntrans",			S_OBJ_REF(modes[KEYMODE_CONNTRANS]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"hold,endcall,transfer,conf,park,select,dirtrfr,monitor,vidmode,meetme,cfwdall,cfwdbusy","displayed when we are connected and could transfer a call"},
-	{"digitsfoll",			S_OBJ_REF(modes[KEYMODE_DIGITSFOLL]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"back,endcall,dial",									"displayed when one or more digits have been entered, more are expected"},
-	{"connconf",			S_OBJ_REF(modes[KEYMODE_CONNCONF]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"conflist,newcall,endcall,hold,vidmode,monitor",					"displayed when we are in a conference"},
-	{"ringout",			S_OBJ_REF(modes[KEYMODE_RINGOUT]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"empty,endcall,transfer",								"displayed when We are calling someone"},
-	{"offhookfeat",			S_OBJ_REF(modes[KEYMODE_OFFHOOKFEAT]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"resume,newcall,endcall",								"displayed wenn we went offhook using a feature"},
-	{"onhint",			S_OBJ_REF(modes[KEYMODE_INUSEHINT]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial,newcall,pickup,gpickup",							"displayed when a hint is activated"},
-	{"onstealable",			S_OBJ_REF(modes[KEYMODE_ONHOOKSTEALABLE]),TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial,newcall,barge,intrcpt,cfwdall,pickup,gpickup,dnd",				"displayed when there is a call we could steal on one of the neighboring phones"},
-	{"holdconf",			S_OBJ_REF(modes[KEYMODE_HOLDCONF]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"resume,newcall,endcall,join",								"displayed when we are a conference moderator, have the conference on hold and have another active call"},
+	{"onhook",			S_OBJ_REF(modes[KEYMODE_ONHOOK]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial, newcall, cfwdall, dnd, pickup, gpickup, private",					"displayed when we are on hook"},
+	{"connected",			S_OBJ_REF(modes[KEYMODE_CONNECTED]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"hold, endcall, park, vidmode, select, cfwdall, cfwdbusy, idivert, monitor",			"displayed when we have a connected call"},
+	{"onhold",			S_OBJ_REF(modes[KEYMODE_ONHOLD]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"resume, newcall, endcall, transfer, conflist, select, dirtrfr, idivert, meetme",		"displayed when we have a call on hold"},
+	{"ringin",			S_OBJ_REF(modes[KEYMODE_RINGIN]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"answer, endcall, transvm, idivert",							"displayed when we have an incoming call"},
+	{"offhook",			S_OBJ_REF(modes[KEYMODE_OFFHOOK]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial, endcall, private, cfwdall, cfwdbusy, pickup, gpickup, meetme, barge",			"displayed when the phone is taken off hook"},
+	{"conntrans",			S_OBJ_REF(modes[KEYMODE_CONNTRANS]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"hold, endcall, transfer, conf, park, select, dirtrfr, monitor, vidmode, meetme, cfwdall, cfwdbusy","displayed when we are connected and could transfer a call"},
+	{"digitsfoll",			S_OBJ_REF(modes[KEYMODE_DIGITSFOLL]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"back, endcall, dial",									"displayed when one or more digits have been entered, more are expected"},
+	{"connconf",			S_OBJ_REF(modes[KEYMODE_CONNCONF]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"conflist, newcall, endcall, hold, vidmode, monitor",					"displayed when we are in a conference"},
+	{"ringout",			S_OBJ_REF(modes[KEYMODE_RINGOUT]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"empty, endcall, transfer",								"displayed when We are calling someone"},
+	{"offhookfeat",			S_OBJ_REF(modes[KEYMODE_OFFHOOKFEAT]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"resume, newcall, endcall",								"displayed wenn we went offhook using a feature"},
+	{"onhint",			S_OBJ_REF(modes[KEYMODE_INUSEHINT]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial, newcall, pickup, gpickup",							"displayed when a hint is activated"},
+	{"onstealable",			S_OBJ_REF(modes[KEYMODE_ONHOOKSTEALABLE]),TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"redial, newcall, barge, intrcpt, cfwdall, pickup, gpickup, dnd",				"displayed when there is a call we could steal on one of the neighboring phones"},
+	{"holdconf",			S_OBJ_REF(modes[KEYMODE_HOLDCONF]), 	TYPE_STRING,									SCCP_CONFIG_FLAG_NONE,						SCCP_CONFIG_NOUPDATENEEDED,		"resume, newcall, endcall, join",								"displayed when we are a conference moderator, have the conference on hold and have another active call"},
 	{"uriaction", 			S_OBJ_REF(softkeyCbMap),		TYPE_STRING,									SCCP_CONFIG_FLAG_MULTI_ENTRY,					SCCP_CONFIG_NOUPDATENEEDED, 		NULL,											"softkey uri action to replace default handling. Format: uriaction = softkeyname, uri[,uri...]\n. URI can be an embedded cisco action (like Key:Service, Play:1041.raw) or a URL"
 																																												"If uri is a url the following parameters will be added to it: devicename, linename, channelname, callid, linkedid, uniqueid, appid, transactionid"},
 };

commit 8ffc52dbe664acc2a35dca19690916bc8d2281c7

Fixup! 09406a50
Which caused softkeysets with numerous EMPTY keys.

Signed-off-by: Diederik de Groot <dkgroot@talon.nl>

src/sccp_config.c
@@ -2875,25 +2875,26 @@ static int sccp_config_getSoftkeyLbl(char *key)
  * \param data configuration values
  * \return number of parsed softkeys
  */
-static uint8_t sccp_config_readSoftSet(uint8_t * softkeyset, const char *data)
+static uint8_t sccp_config_readSoftKeySet(uint8_t * softkeyset, const char * data)
 {
 	if (!data) {
 		return 0;
 	}
-
 	int i = 0, j;
-	char * labels = NULL;
-	char * splitter = NULL;
-	char * label = NULL;
 	int softkey;
 
-	labels = pbx_strdupa(data);
-	splitter = labels;
-	while ((label = strsep(&splitter, ",")) != NULL) {
-		label = pbx_trim_blanks(label);
+	char * labels = pbx_strdupa(data);
+	char * labelrest = NULL;
+	char delims[] = ",";
+
+	char * label = strtok_r(labels, delims, &labelrest);
+	while(label) {
+		pbx_log(LOG_NOTICE, "SCCP: readSoftKeySet: label:%s ->%s\n", label, pbx_trim_blanks(label));
+		label = pbx_strip(label);
 		if ((softkey = sccp_config_getSoftkeyLbl(label)) != -1 && (i + 1) < StationMaxSoftKeySetDefinition) {
 			softkeyset[i++] = softkey;
 		}
+		label = strtok_r(NULL, delims, &labelrest);
 	}
 	/*! \todo we used calloc to create this structure, so setting EMPTY should not be required here */
 	for (j = i; j < StationMaxSoftKeySetDefinition; j++) {
@@ -3000,7 +3001,7 @@ void sccp_config_softKeySet(PBX_VARIABLE_TYPE * variable, const char *name)
 
 			/* add new value */
 			uint8_t *softkeyset = (uint8_t *)sccp_calloc(StationMaxSoftKeySetDefinition, sizeof(uint8_t));
-			keySetSize = sccp_config_readSoftSet(softkeyset, variable->value);
+			keySetSize = sccp_config_readSoftKeySet(softkeyset, variable->value);
 			//sccp_log((DEBUGCAT_CONFIG + DEBUGCAT_SOFTKEY)) (VERBOSE_PREFIX_3 "Adding KeyMode(%d), with Size(%d), prt(%p) to Softkeyset: %s\n", keyMode, keySetSize, softkeyset, name);
 			if (keySetSize > 0) {
 				softKeySetConfiguration->modes[keyMode].id = keyMode;