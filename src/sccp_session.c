31c25a89d4a288696aee78edced32125e2190450
Diederik de Groot <dkgroot@talon.nl>
2021-01-28 16:52:53 +0100

    Fix: Calling function process_buffer taints argument msg.header. (Coverity)
    Signed-off-by: Diederik de Groot <dkgroot@talon.nl>

--- src/sccp_session.c
+++ src/sccp_session.c
@@ -405,8 +405,7 @@ static gcc_inline int process_buffer(sccp_session_t * s, sccp_msg_t * msg, unsig
 {
 	int res = 0;
 	while (*len >= SCCP_PACKET_HEADER && *len <= SCCP_MAX_PACKET * 2) {										// We have at least SCCP_PACKET_HEADER, so we have the payload length
-		uint32_t hdr_len = buffer[0] | (buffer[1] << 8) | (buffer[2] << 16) | (buffer[3] << 24);
-		uint32_t payload_len = letohl(hdr_len) + (SCCP_PACKET_HEADER - 4);
+		uint32_t payload_len = letohl(*(uint32_t *)buffer) + (SCCP_PACKET_HEADER - 4);
 		if (*len < payload_len) {
 			break;												// Too short - haven't received whole payload yet, go poll for more
 		}
