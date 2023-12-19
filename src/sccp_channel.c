fd84466cb8d1e4b6cca4fe282a4fef5b78edb9d7
David Dederscheck <davidded@davidded.de>
2023-12-19 01:03:53 +0100

    refactoring of holepunch, initial refactored implementation still lacking proper comments and cleanup

--- src/sccp_channel.c
+++ src/sccp_channel.c
@@ -809,10 +809,10 @@ void sccp_channel_startHolePunch(constChannelPtr c)
 	*/
 	// No checks necessary here, since everything gets called exactly in one place, and that is when one-way-audio is requested in progress.
 	//if(!sccp_rtp_getState(audio, SCCP_RTP_TRANSMISSION) && pbx_channel_state(c->owner) != AST_STATE_UP && c->wantsEarlyRTP()) {
-		sccp_log(DEBUGCAT_RTP)(VERBOSE_PREFIX_3 "%s: (%s) start Punching a hole through the firewall (if necessary)\n", c->designator, __func__);
+		sccp_log(DEBUGCAT_RTP)(VERBOSE_PREFIX_3 "%s: (%s) start Punching a hole through the firewall\n", c->designator, __func__);
 		c->privateData->firewall_holepunch = TRUE;
 	/* start audio rtp server if not already present */
-		if (!c->rtp.audio.instance && !sccp_rtp_createServer(d, c, SCCP_RTP_AUDIO)) {
+		if (!c->rtp.audio.instance && !sccp_rtp_createServer(d, (channelPtr)c, SCCP_RTP_AUDIO)) {
 			pbx_log(LOG_WARNING, "%s: Error opening RTP instance for channel %s\n", d->id, c->designator);
 		}
 		sccp_channel_startMediaTransmission(c);
